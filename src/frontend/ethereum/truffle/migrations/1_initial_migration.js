ada7e66bc11e9da58145862aaea9b29c0187bc92
lucaspelloni2 <lucas_pelloni@hotmail.com>
2018-06-26 14:25:07 +0200

    module.exports for deploy

--- src/frontend/ethereum/truffle/migrations/1_initial_migration.js
+++ src/frontend/ethereum/truffle/migrations/1_initial_migration.js
@@ -1,14 +1,19 @@
 const eureka = artifacts.require('./eureka.sol');
-
 const utils = artifacts.require('./Utils.sol');
-
 const safemath = artifacts.require('./SafeMath.sol');
 
-module.exports = async deployer => {
+let EUREKA_CONTRACT_ADDRESS = '';
+
+const deploy = async deployer => {
   // overwrite: false --> Don't deploy this contract if it has already been deployed
   await deployer.deploy(utils, {overwrite: false});
   await deployer.deploy(safemath, {overwrite: false});
   await deployer.link(utils, eureka);
   await deployer.link(safemath, eureka);
-  await deployer.deploy(eureka, {overwrite: false});
+  deployer.deploy(eureka, {overwrite: false}).then(contract => {
+    EUREKA_CONTRACT_ADDRESS = contract.address;
+  });
 };
+
+module.exports = deploy;
+exports.EUREKA_CONTRACT_ADDRESS = EUREKA_CONTRACT_ADDRESS;
