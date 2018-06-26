1d2f901ce6b969ae9e3dac519144164058429db5
lucaspelloni2 <lucas_pelloni@hotmail.com>
2018-06-26 12:00:56 +0200

    truffle initial migration - overwrite: false

--- src/frontend/ethereum/truffle/migrations/1_initial_migration.js
+++ src/frontend/ethereum/truffle/migrations/1_initial_migration.js
@@ -1,5 +1,14 @@
-var Migrations = artifacts.require("./Migrations.sol");
+const eureka = artifacts.require('./eureka.sol');
 
-module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+const utils = artifacts.require('./Utils.sol');
+
+const safemath = artifacts.require('./SafeMath.sol');
+
+module.exports = async deployer => {
+  // overwrite: false --> Don't deploy this contract if it has already been deployed
+  await deployer.deploy(utils, {overwrite: false});
+  await deployer.deploy(safemath, {overwrite: false});
+  await deployer.link(utils, eureka);
+  await deployer.link(safemath, eureka);
+  await deployer.deploy(eureka, {overwrite: false});
 };
--- src/frontend/ethereum/truffle/truffle.js
+++ src/frontend/ethereum/truffle/truffle.js
@@ -15,4 +15,11 @@
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: 'localhost',
+      port: 7545, // your can use your port number
+      network_id: '*' // Match any network id
+    }
+  }
 };
