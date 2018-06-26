a3113955767a0e5d3427a3a036865304e660812b
Andreas Schaufelbühl <andreas.schaufelbuehl@gmail.com>
2018-06-26 16:49:55 +0200

    change review post to proper post-request

--- src/backend/api/api.mjs
+++ src/backend/api/api.mjs
@@ -2,8 +2,10 @@ import express from 'express'
 import DBConnector from "../db/db";
 import {asyncHandler} from "./requestHandler";
 import Web3Manager from "../web3/index";
+import bodyParser from 'body-parser';
 
 const app = express();
+app.use(bodyParser.json());
 
 // web3 instance
 const web3Manager = new Web3Manager();
@@ -13,13 +15,15 @@ web3Manager.deployContract();
 const dbUrl = 'localhost/eurekaDB';
 const db = new DBConnector(dbUrl);
 
+
 app.get('/reviews', asyncHandler(async (req, res) => {
     let result = await db.getAllReviews();
     return result;
 }));
 
-app.get('/insertReview', asyncHandler(async (req, res) => {
-    let result = await db.insertReview(3, 'test');
+app.post('/reviews', asyncHandler(async (req, res) => {
+    const rating = parseInt(req.body.rating);
+    let result = await db.insertReview(rating, req.body.text);
     return result;
 }));
 
