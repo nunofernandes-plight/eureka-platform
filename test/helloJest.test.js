5967fbe4ecd60d149eee5b5b761bbb7ca31653a7
severinwullschleger <severin.wullschleger@bluewin.ch>
2018-07-02 14:42:52 +0200

    first jest test

---
+++ test/helloJest.test.js
@@ -0,0 +1,10 @@
+// const sum = require('./sum');
+
+// test('adds 1 + 2 to equal 3', () => {
+//   expect(sum(1, 2)).toBe(3);
+// });
+
+test('adds 1 + 2 to equal 3', () => {
+  console.log("test");
+  // expect(3).toBe(3);
+});
