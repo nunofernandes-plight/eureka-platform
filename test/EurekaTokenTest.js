dd4a4c8d00a5fe410e18c46fcb3ad51b65b8b066
severinwullschleger <severin.wullschleger@bluewin.ch>
2018-07-05 17:16:33 +0200

    new tests with plain web3

--- test/EurekaTokenTest.js
+++ test/EurekaTokenTest.js
@@ -2,55 +2,118 @@ import test from 'ava';
 import deployContracts from '../src/backend/web3/index.mjs';
 import getAccounts from "../src/backend/web3/get-accounts.mjs";
 
-test('minting', async t => {
+let contract;
+let accounts;
+test.beforeEach(async t => {
   const [eurekaContract] = await deployContracts();
+  contract = eurekaContract;
+  accounts = await getAccounts();
+});
 
-  let accounts = await getAccounts();
+test.only('minting and total supply', async t => {
   let amounts = [];
 
+  let amount = 1000;
   accounts.forEach(() => {
-    amounts.push(1000);
+    amounts.push(amount);
   });
 
-  await getTotalSupply(accounts[0], eurekaContract);
+  //initial Supply
+  let initialTotalSupply = await getTotalSupply(accounts[0]);
+  t.is(parseInt(initialTotalSupply), 0, 'should be 0');
 
-  let gasEstimated = await eurekaContract.methods.mint(accounts, amounts)
+  let gasEstimated = await contract.methods.mint(accounts, amounts)
     .estimateGas({
       from: accounts[0]
     });
 
- await eurekaContract.methods
+  //Minting
+ await contract.methods
    .mint(accounts, amounts)
    .send({
       from: accounts[0],
       gas: gasEstimated
     })
-   .on('receipt', async receipt => {
-     console.log(receipt);
+   .then((receipt) => {
      return receipt;
     });
 
-  const totalSupply = await getTotalSupply(accounts[0], eurekaContract);
+  // total supply after minting
+  const totalSupply = await getTotalSupply(accounts[0]);
+  t.is(parseInt(totalSupply), amounts.length * amount, 'should be 1000 * 10');
+  //
+  // // try to tranfer
+  // t.throws(async () => {
+  //   await contract.methods
+  //     .transfer(accounts[1], 1000)
+  //     .send({
+  //       from: accounts[0]
+  //     })
+  //     .then((err) => {
+  //       return err;
+  //     })
+  // });
 
-  t.is(totalSupply, (amounts.length * 1000).toString());
+  await contract.methods
+    .finishMinting()
+    .send({
+      from: accounts[0]
+    })
+    .then((receipt) => {
+      return receipt;
+    });
+
+
+
+
+
 });
 
-function getTotalSupply(account, eurekaContract) {
-  return eurekaContract.methods
+test('minting and balanceOf', async t => {
+  let amounts = [];
+
+  let amount = 1000;
+  accounts.forEach(() => {
+    amounts.push(amount);
+  });
+
+  let balance = await getBalanceOf(accounts[0]);
+  t.is(parseInt(balance), 0, 'should be 0');
+
+  let gasEstimated = await contract.methods.mint(accounts, amounts)
+    .estimateGas({
+      from: accounts[0]
+    });
+
+  //Minting
+  await contract.methods
+    .mint(accounts, amounts)
+    .send({
+      from: accounts[0],
+      gas: gasEstimated
+    })
+    .then((receipt) => {
+      return receipt;
+    });
+
+  balance = await getBalanceOf(accounts[0]);
+  t.is(parseInt(balance), 1000, 'should be 1000');
+});
+
+function getTotalSupply(account) {
+  return contract.methods
     .totalSupply()
     .call({from: account})
     .then(succ => {
-      console.log(succ);
       return succ;
     });
 }
 
-function getBalanceOf(account, eurekaContract) {
-  return eurekaContract.methods
+function getBalanceOf(account) {
+  return contract.methods
     .balanceOf(account)
     .call({from: account})
     .then((bal) => {
-      console.log(bal);
       return bal;
     })
     .catch(err => console.log(err));
