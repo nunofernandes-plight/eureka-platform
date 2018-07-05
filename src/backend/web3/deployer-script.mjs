3268afc755428059bb1748cd70d54f0a4345d509
severinwullschleger <severin.wullschleger@bluewin.ch>
2018-07-05 15:05:52 +0200

    clean up deployment script

--- src/backend/web3/deployer-script.mjs
+++ src/backend/web3/deployer-script.mjs
@@ -5,23 +5,7 @@ const deploy = async () => {
   // deployContracts will deploy all libraries specified in the input file and once they
   // get a valid Ethereum address, all the smart contracts gets also deployed.
   // the method returns a web3 instance of the smart contract itself.
-  const [eurekaContract] = await deployContracts();
-  //methods(eurekaContract);
-};
-
-const methods = async eurekaContract => {
-  const accounts = await getAccounts();
-  const totalSupply = await totalSupply(accounts, eurekaContract);
-  console.log(totalSupply);
-};
-
-const totalSupply = async (accounts, eurekaContract) => {
-  return eurekaContract.methods
-    .totalSupply()
-    .call({from: accounts[0]})
-    .then(succ => {
-      return succ;
-    });
+  await deployContracts();
 };
 
 deploy();
