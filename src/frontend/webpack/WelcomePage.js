6498ead3cd05a5a4f03b3faea906ef229a4224bd
lucaspelloni2 <lucas_pelloni@hotmail.com>
2018-07-06 13:41:51 +0200

    colors palette react component

--- src/frontend/webpack/WelcomePage.js
+++ src/frontend/webpack/WelcomePage.js
@@ -1,30 +1,17 @@
 import React, {Component} from 'react';
 import styled from 'styled-components';
-import EurekaLogo from "./Icons/EurekaLogo.js";
+import EurekaLogo from './Icons/EurekaLogo.js';
 
 const Container = styled.div`
-  width: 100%;
-  height: 100vh;
-  position: relative;
-  overflow: hidden;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
 `;
 
-const BigTitle = styled.h1`
-  font-size: 5vw;
-  text-transform: uppercase;
-`;
 class WelcomePage extends Component {
   render() {
-    return (
-      <Container>
-        <BigTitle>Eureka Platform</BigTitle>
-        <EurekaLogo width="180"/>
-      </Container>
-    );
+    return <Container />;
   }
 }
 
