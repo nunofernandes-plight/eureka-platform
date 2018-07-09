5a169e6d04057ceac061dcab0e930b68c1c40448
lucaspelloni2 <lucas_pelloni@hotmail.com>
2018-07-09 14:37:41 +0200

    first header draft

--- src/frontend/webpack/Header.js
+++ src/frontend/webpack/Header.js
@@ -2,19 +2,87 @@ import React, {Component} from 'react';
 import styled from 'styled-components';
 import {Row} from '../helpers/layout.js';
 import EurekaLogo from './Icons/EurekaLogo.js';
+import {__METAMASK, __THIRD} from '../helpers/colors.js';
+import Icon from './Icons/Icon.js';
+import MetaMaskLogo from './Icons/MetaMaskLogo.js';
 
 const Container = Row.extend`
-  min-height: 85px;
+  transition: all 150ms ease;
+  color: ${__THIRD};
+  cursor: pointer;
+  font-size: 18px;
+  padding: 25px;
+  align-items: center;
+  justify-content: space-between;
+`;
+const LeftContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+`;
+
+const MiddleContainer = styled.div`
+  display: flex;
   align-items: center;
 `;
 
+const RightContainer = styled.div`
+  display: flex;
+`;
+
+const Item = styled.div`
+  margin: 0 10px;
+  align-self: center;
+`;
+
+const MetaMask = Item.extend`
+  display: flex;
+  align-items: center;
+  font-size: 13px;
+  background: #2f3292;
+  color: white;
+  padding-top: 6px;
+  padding-bottom: 6px;
+  padding-left: 10px;
+  padding-right: 4px;
+  border-radius: 6px;
+`;
+const SignUp = Item.extend`
+  border: 1px solid ${__THIRD};
+  padding: 8px 12px;
+  border-radius: 4px;
+`;
+
 const renderLeft = () => {
-  return <EurekaLogo height={44} blue />;
+  return (
+    <LeftContainer>
+      <EurekaLogo height={44} blue />
+    </LeftContainer>
+  );
 };
 
-const renderMiddle = () => {};
+const renderMiddle = () => {
+  return (
+    <MiddleContainer>
+      <Item>
+        Products <Icon icon="chevron-down" width={15} height={15} />
+      </Item>
+      <MetaMask>
+        Get MetaMask{' '}
+        <MetaMaskLogo style={{marginRight: 5}} width={15} height={15} />
+      </MetaMask>
+    </MiddleContainer>
+  );
+};
 
-const renderRight = () => {};
+const renderRight = () => {
+  return (
+    <RightContainer>
+      <Item>Login</Item>
+      <SignUp>Sign Up</SignUp>
+    </RightContainer>
+  );
+};
 
 class Header extends Component {
   render() {
