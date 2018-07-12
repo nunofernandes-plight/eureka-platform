418e0ee52726153e83a80e6e7bad63d342d484c0
Andreas Schaufelbühl <andreas.schaufelbuehl@gmail.com>
2018-07-12 13:05:36 +0200

    implementend signup and login endpoints with hash-function

--- src/backend/api/api.mjs
+++ src/backend/api/api.mjs
@@ -3,51 +3,60 @@ import bodyParser from 'body-parser';
 import passport from 'passport';
 import dotenv from 'dotenv';
 
+
 import router from '../routes/index.mjs';
 import passportLocal from "passport-local/lib/index";
 import userSchema from "../schemas/user";
 import mongoose from "../db/mongoose";
+import hasher from '../helpers/hasher'
 
-dotenv.config();
 const LocalStrategy = passportLocal.Strategy;
+
+//load in env variables
+dotenv.config();
+
 /**
  *  Configure the local strategy for use by Passport
  **/
 const User = mongoose.model('users', userSchema, 'users');
 
 passport.use(new LocalStrategy(
-  function(username, password, done) {
+   async function (username, password, done) {
+     const dbUser = await User.findOne({'username': username});
+    const isCorrectHash =  await hasher.compare(password, dbUser.password);
+
     User.findOne({
       username: username
-    }, function(err, user) {
+    }, function (err, user) {
       if (err) {
         return done(err);
       }
 
       // no user found
-      if(!user) {
+      if (!user) {
         return done(null, false);
       }
 
       // incorrect password
-      if (user.password != password) {
+      if (!isCorrectHash) {
         return done(null, false);
       }
 
       //login success
       return done(null, user);
     });
-  }
-));
+  })
+)
+;
 
 /**
  *  Configure Passport authenticated session persistence.
  */
-passport.serializeUser(function(user, done) {
+passport.serializeUser(function (user, done) {
   done(null, user.id);
 });
 
-passport.deserializeUser(function(id, done) {
+passport.deserializeUser(function (id, done) {
   User.findById(id, function (err, user) {
     done(err, user);
   })
@@ -58,15 +67,29 @@ app.use(passport.initialize());
 app.use(bodyParser.json());
 
 
-app.post('/', passport.authenticate('local', { failureRedirect: '/error' }),
-  function(req, res) {
-  res.redirect('/success?username='+req.user.username);
+app.post('/login', passport.authenticate('local', {failureRedirect: '/error'}),
+  function (req, res) {
+    res.redirect('/success?username=' + req.user.username);
   });
 // api routes
+app.post('/signup', async (req, res, next) => {
+  console.log(req.body.password);
+  let hashedPassword = await hasher.hash(req.body.password);
+  console.log('Hash from API: ' + hashedPassword);
+  const user = new User({
+      "username": req.body.username,
+      "password": hashedPassword
+  })
+  user.save(function (err) {
+    if(err) throw err;
+    console.log('New User saved on DB')
+    return res.send('New User sign up in DB')
+  })
+});
 app.use('/api', router);
 
 //testing
-app.get('/success', (req, res) => res.send("Welcome "+req.query.username+"!!"));
+app.get('/success', (req, res) => res.send("Welcome " + req.query.username + "!!"));
 app.get('/error', (req, res) => res.send("error logging in"));
 
 export default app;
