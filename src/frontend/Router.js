505e5ad61ddac05cd20226f14f781237e60ad81f
lucaspelloni2 <lucas_pelloni@hotmail.com>
2018-07-12 11:47:11 +0200

    padding top on header

--- src/frontend/Router.js
+++ src/frontend/Router.js
@@ -3,7 +3,7 @@ import {Switch, Route} from 'react-router';
 import {BrowserRouter} from 'react-router-dom';
 import WelcomePage from './webpack/login/WelcomePage';
 import Header from './webpack/Header';
-import Login from './webpack/login/Login.js';
+import Login from './webpack/login/Login';
 
 class Router extends Component {
   constructor() {
@@ -13,12 +13,14 @@ class Router extends Component {
     return (
       <div>
         <Header />
-        <BrowserRouter>
-          <Switch>
-            <Route path="/login" exact render={() => <Login />} />
-            <Route path="/" exact render={() => <WelcomePage />} />
-          </Switch>
-        </BrowserRouter>
+        <div style={{paddingTop: 100}}>
+          <BrowserRouter>
+            <Switch>
+              <Route path="/login" exact render={() => <Login />} />
+              <Route path="/" exact render={() => <WelcomePage />} />
+            </Switch>
+          </BrowserRouter>
+        </div>
       </div>
     );
   }
