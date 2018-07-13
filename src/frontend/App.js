f710b7dd2a8d9b931543f9e41996f36ec990f2f6
lucaspelloni2 <lucas_pelloni@hotmail.com>
2018-07-13 15:05:15 +0200

    allow login if metamask is installed

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -1,10 +1,31 @@
 import React, {Component} from 'react';
 import Router from './Router';
+import Web3Providers from './web3/Web3Providers.js';
+import Web3 from 'web3';
+
 class App extends Component {
+  constructor() {
+    super();
+    let web3 = window.web3;
+    let provider;
+    if (typeof web3 !== 'undefined' && web3.currentProvider.isMetaMask) {
+      // MetaMask as main provider
+      console.info('MetaMask detected in this browser');
+      web3 = new Web3(web3.currentProvider);
+      provider = Web3Providers.META_MASK;
+    } else {
+      // TODO: fallback strategy
+    }
+    this.state = {
+      web3,
+      provider
+    };
+  }
+
   render() {
     return (
       <div>
-        <Router />
+        <Router web3={this.state.web3} provider={this.state.provider} />
       </div>
     );
   }
