cb07f4a9e466648351cdf15bc59a89bd31c873a8
Andreas Schaufelbühl <andreas.schaufelbuehl@gmail.com>
2018-07-22 11:54:23 +0200

    first draft for email service

--- src/backend/index.mjs
+++ src/backend/index.mjs
@@ -1,11 +1,15 @@
 import app from './api/api.mjs';
 import dotenv from 'dotenv';
+import mailService from './helpers/mailService';
 
 import isProduction from '../helpers/isProduction.mjs';
+
+mailService;
 
 if (!isProduction()) {
   //import env variables from .env file
   dotenv.config();
   app.listen(process.env.PORT || 8080);
 }
+
 console.log('App started. Date: ' + new Date().toString());
