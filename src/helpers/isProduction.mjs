0944658ebfe84a0ee4011c97cc6e613bc2755cf2
Andreas Schaufelbühl <andreas.schaufelbuehl@gmail.com>
2018-07-31 12:32:01 +0200

    excluded the db-test

--- src/helpers/isProduction.mjs
+++ src/helpers/isProduction.mjs
@@ -7,11 +7,14 @@ export const isProduction = () => {
   return process.env.NODE_ENV === 'production';
 };
 
+export const isTesting = () => {
+  // for client-side --> TODO
+  // for server-side
+  return process.env.NODE_ENV === 'test';
+};
+
 export const isDevelopment = () => {
-  // for client-side
-  if (typeof window !== 'undefined') {
-    return window.location.hostname === 'localhost';
-  }
+  // for client-side --> TODO
   // for server-side
   return process.env.NODE_ENV === 'development';
 };
