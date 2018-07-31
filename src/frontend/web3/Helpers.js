fed5b302c7cf1f225ab10aa7d293026704a98bcf
lucaspelloni2 <lucas_pelloni@hotmail.com>
2018-07-31 14:51:43 +0200

    get all accounts as map function

--- src/frontend/web3/Helpers.js
+++ src/frontend/web3/Helpers.js
@@ -24,3 +24,25 @@ export const getBalance = async (web3, address) => {
       );
   }
 };
+
+export const getAllAccounts = async web3 => {
+  let accounts = new Map();
+  if (web3) {
+    return web3.eth
+      .getAccounts()
+      .then(async addresses => {
+        await Promise.all(
+          addresses.map(async address => {
+            const balance = await getBalance(web3, address);
+            const ethBalance = web3.utils.fromWei(balance.toString()); 
+            accounts.set(address, ethBalance);
+          })
+        );
+        return accounts;
+      })
+      .catch(err => {
+        console.error('An error with getAccounts() occurred: ' + err);
+        return null;
+      });
+  }
+};
