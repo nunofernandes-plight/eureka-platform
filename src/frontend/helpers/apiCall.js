9cafa784ab46fe21504c452f86d92d9498b28318
lucaspelloni2 <lucas_pelloni@hotmail.com>
2018-08-02 17:00:27 +0200

    api call component adjusted

--- src/frontend/helpers/apiCall.js
+++ src/frontend/helpers/apiCall.js
@@ -1,30 +1,21 @@
 const ApiCall = (url, options = {}) => {
-    options.credentials = 'include';
-    options.headers = new Headers({
-        'CSRF-Token': window._csrf,
-        'Content-Type': 'application/json'
-    });
-    return new Promise((resolve, reject) => {
-        fetch(url, options)
-            .then(response => {
-                if (response.status === 500) {
-                    throw new Error('Server is offline');
-                }
-                if (response.status === 404) {
-                    throw new Error('Not found');
-                }
-                return response.json();
-            })
-            .then(response => {
-                if (!response.success) {
-                    const err = new Error(response.error);
-                    err.code = response.code;
-                    throw err;
-                }
-                resolve(response);
-            })
-            .catch(reject);
-    });
+  options.headers = new Headers({
+    'Content-Type': 'application/json'
+  });
+  return new Promise((resolve, reject) => {
+    fetch(url, options)
+      .then(response => {
+        console.log(response.body);
+        if (response.status === 500) {
+          throw new Error('Server is offline');
+        }
+        if (response.status === 404) {
+          throw new Error('Not found');
+        }
+        return response;
+      })
+      .catch(reject);
+  });
 };
 
 export default ApiCall;
