45a88f51c7688307d64ccf69ea6aeb622f2a4de5
severinwullschleger <severin.wullschleger@bluewin.ch>
2018-08-02 15:40:18 +0200

    added async handler for createUser method

--- src/backend/routes/register-routes.mjs
+++ src/backend/routes/register-routes.mjs
@@ -1,30 +1,20 @@
 import express from 'express';
 import path from 'path';
 import userService from '../db/user-service.mjs';
+import {asyncHandler} from "../api/requestHandler.mjs";
 
 const router = express.Router();
 const __dirname = path.resolve();
 
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/src/backend/view/register.html'));
 });
 
-router.post('/', async function(req, res) {
-  userService
-    .createUser(req.body.password, req.body.email, req.body.ethereumAddress)
-    .then(newUserInDB => {
-      req.login(newUserInDB._id, function(err) {
-        if (err) {
-          res.status(400);
-          res.send('Login error: ' + err);
-        }
-        res.send(newUserInDB);
-      });
-    })
-    .catch(err => {
-      res.status(400);
-      res.send('Registration error: ' + err);
-    });
-});
+router.post('/',
+  asyncHandler(async (req, res) => {
+    return userService
+      .createUser(req.body.password, req.body.email, req.body.ethereumAddress);
+  })
+);
 
 export default router;
