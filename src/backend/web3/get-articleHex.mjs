b5a660c5384c379d87ed86eee89de6f6a387c857
severinwullschleger <severin.wullschleger@bluewin.ch>
2018-08-03 16:13:08 +0200

    changed  get Article Hex method

--- src/backend/web3/get-articleHex.mjs
+++ src/backend/web3/get-articleHex.mjs
@@ -1,55 +1,54 @@
 import web3 from './web3Instance.mjs';
 
 const getArticleHex = article => {
-  // convert the articleVersion test data to hex strings
-  let articleHashHex = '0x' + article.articleHash;
-  let urlHex64 = web3.utils.padRight(web3.utils.toHex(article.url), 64);
 
-  // length of authors array
-  let authorsLengthHex4 = web3.utils.padLeft(   // for number add padLeft instead of right
-    web3.utils.toHex(article.authors.length),
-    4
-  );
-  // authors contribution ratio
-  let contributorsHex4 = [];
-  article.contributorRatios.forEach(ratio => {
-    contributorsHex4.push(web3.utils.padLeft(
-      web3.utils.toHex(ratio),
-      4
-    ));
-  });
-
-  // length of linked articles array
-  let linkedArticleLengthHex4 = web3.utils.padLeft(
-    web3.utils.toHex(article.linkedArticles.length),
-    4
-  );
-  // linked articles split ratio
-  let linkedArticlesRatioHex4 = [];
-  article.linkedArticlesSplitRatios.forEach(ratio => {
-    linkedArticlesRatioHex4.push(web3.utils.padLeft(
-      web3.utils.toHex(ratio),
-      4
-    ));
-  });
-
+  let amountOfArrays = 4;
+  let arrayDataStartByte = 32 + 32 + amountOfArrays * 32;
 
   /*
    connect the hex strings
     */
-  let dataInHex =
-    articleHashHex + urlHex64.substring(2) + authorsLengthHex4.substring(2);
+  let dataInHex = '0x'
+    + article.articleHash
+    + web3.utils.padRight(web3.utils.toHex(article.url), 64).substring(2);
 
-  let i = 0;
+  dataInHex += web3.utils.padLeft(web3.utils.toHex(arrayDataStartByte), 64).substring(2);
+  arrayDataStartByte += 32 * (article.authors.length + 1);
+
+  dataInHex += web3.utils.padLeft(web3.utils.toHex(arrayDataStartByte), 64).substring(2);
+  arrayDataStartByte += 32 * (article.contributorRatios.length + 1);
+
+  dataInHex += web3.utils.padLeft(web3.utils.toHex(arrayDataStartByte), 64).substring(2);
+  arrayDataStartByte += 32 * (article.linkedArticles.length + 1);
+
+  dataInHex += web3.utils.padLeft(web3.utils.toHex(arrayDataStartByte), 64).substring(2);
+  arrayDataStartByte += 32 * (article.linkedArticlesSplitRatios.length + 1);
+
+  //authors address
+  dataInHex += web3.utils.padLeft(web3.utils.toHex(article.authors.length), 64).substring(2);
   article.authors.forEach(address => {
-    dataInHex = dataInHex + address.substring(2) + contributorsHex4[i++].substring(2);
+    dataInHex += web3.utils.padLeft(address, 64).substring(2);
   });
 
-  i = 0;
-  dataInHex += linkedArticleLengthHex4.substring(2);
-  article.linkedArticles.forEach(linkedArticleHash => {
-    dataInHex = dataInHex + linkedArticleHash + linkedArticlesRatioHex4[i++].substring(2);
+  //contributorRatio
+  dataInHex += web3.utils.padLeft(web3.utils.toHex(article.contributorRatios.length), 64).substring(2);
+  article.contributorRatios.forEach(ratio => {
+    dataInHex += web3.utils.padLeft(web3.utils.toHex(ratio), 64).substring(2);
   });
+
+  //linked articles array
+  dataInHex += web3.utils.padLeft(web3.utils.toHex(article.linkedArticles.length), 64).substring(2);
+  article.linkedArticles.forEach(hash => {
+    dataInHex += hash
+  });
+
+  //linked articles split ratio
+  dataInHex += web3.utils.padLeft(web3.utils.toHex(article.linkedArticlesSplitRatios.length), 64).substring(2);
+  article.linkedArticlesSplitRatios.forEach(ratio => {
+    dataInHex += web3.utils.padLeft(web3.utils.toHex(ratio), 64).substring(2);
+  });
+
+  console.log('Article data hex string: ' + dataInHex);
 
   return dataInHex;
 };
