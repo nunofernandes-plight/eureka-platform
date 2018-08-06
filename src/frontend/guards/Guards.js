30092571e45d7bd6a56ddb01662eec8e79be0df6
lucaspelloni2 <lucas_pelloni@hotmail.com>
2018-08-06 09:57:44 +0200

    guards from props.location

--- src/frontend/guards/Guards.js
+++ src/frontend/guards/Guards.js
@@ -3,7 +3,9 @@ import {Redirect} from 'react-router-dom';
 
 export const LoginGuard = props => {
   if (!props.authed) {
-    return <Redirect to="/login" />;
+    return (
+      <Redirect to={{pathname: '/login', state: {from: props.location}}} />
+    );
   } else {
     return props.children;
   }
