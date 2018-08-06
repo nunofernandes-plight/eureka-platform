2ca882ae9ae5c1091693a7e2349f4703d548566f
Andy <andreas.schaufelbuehl@gmail.com>
2018-08-06 14:07:40 +0200

    extended submission-service with _id

--- src/backend/db/submission-service.mjs
+++ src/backend/db/submission-service.mjs
@@ -4,8 +4,9 @@ export default {
   getAllSubmissions: () => {
     return ArticleSubmission.find({});
   },
-  createSubmission: (ownerAddress) => {
-    const submission = new ArticleSubmission({ownerAddress: ownerAddress});
+  createSubmission: (ownerAddress, submissionId) => {
+    const submission = new ArticleSubmission(
+      {_id: submissionId, ownerAddress: ownerAddress});
     return submission.save(function(err) {
       if (err) return console.error(err);
       console.log('Created new ArticleSubmission on DB done');
