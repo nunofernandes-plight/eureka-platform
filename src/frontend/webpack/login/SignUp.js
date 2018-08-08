d0e86d4eb67a28a6f914c9f86d4a021444e9e5f8
lucaspelloni2 <lucas_pelloni@hotmail.com>
2018-08-08 12:42:59 +0200

    is address well formed

--- src/frontend/webpack/login/SignUp.js
+++ src/frontend/webpack/login/SignUp.js
@@ -132,12 +132,14 @@ class SignUp extends Component {
       // FAKE PASSWORD FOR DEV
       return '0xb91467e570a6466aa9e9876cbcd013baba02900b8979d43fe208a4a4f339f5fd6007e74cd82e037b800186422fc2da167c747ef045e5d18a5f5d4300f8e1a0291c';
     } else if (this.props.provider === Web3Providers.META_MASK) {
-      return this.props.web3.eth.personal
-        .sign(message, defaultAccount)
-        .then(signedKey => {
-          return signedKey;
-        })
-        .catch(err => console.log(err));
+      if (this.props.web3.utils.isAddress(defaultAccount)) {
+        return this.props.web3.eth.personal
+          .sign(message, defaultAccount)
+          .then(signedKey => {
+            return signedKey;
+          })
+          .catch(err => console.log(err));
+      }
     }
   }
 
