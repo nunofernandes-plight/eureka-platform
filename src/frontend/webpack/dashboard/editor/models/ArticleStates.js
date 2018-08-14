8e691558830d4bb17e7231f083f4b62d57b2d6b0
lucaspelloni2 <lucas_pelloni@hotmail.com>
2018-08-14 12:23:57 +0200

    Article States

---
+++ src/frontend/webpack/dashboard/editor/models/ArticleStates.js
@@ -0,0 +1,31 @@
+export const DRAFT = 'draft';
+export const SUBMITTED = 'submitted';
+export const REJECTED = 'rejected';
+export const PRE_REVIEW = 'pre_review';
+export const IN_REVIEW = 'in_review';
+export const POST_REVIEW = 'post_review';
+export const REVIEWED = 'reviewed';
+export const AWAITING_FOLLOWUP = 'awaiting_followup';
+export const ARCHIVED = 'archived';
+export const OBSOLETE = 'obsolete';
+export const PRE_ACCEPTED = 'pre_accepted';
+export const ACCEPTED = 'accepted';
+export const PRE_PUBLISH = 'pre_publish';
+export const PUBLISHED = 'published';
+
+export const all = {
+    DRAFT,
+    SUBMITTED,
+    REJECTED,
+    PRE_REVIEW,
+    IN_REVIEW,
+    POST_REVIEW,
+    REVIEWED,
+    AWAITING_FOLLOWUP,
+    ARCHIVED,
+    OBSOLETE,
+    PRE_ACCEPTED,
+    ACCEPTED,
+    PRE_PUBLISH,
+    PUBLISHED
+};
