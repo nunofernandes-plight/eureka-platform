2c9c64961dfd0f0edb647651c8325a8b12ff45fb
lucaspelloni2 <lucas_pelloni@hotmail.com>
2018-08-15 14:47:49 +0200

    document renderer

---
+++ src/frontend/webpack/dashboard/editor/DocumentRenderer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {EditorState, convertToRaw} from 'draft-js';
+import Document from '../../../../models/Document.mjs';
+import {deserializeDocument} from '../../../../helpers/documentSerializer.mjs';
+
+export const renderField = (document, field) => {
+  const deserialized = deserializeDocument(new Document(document));
+  if (!document[field] || !(deserialized[field] instanceof EditorState)) {
+    return 'cannot render';
+  }
+
+  switch (field) {
+    case 'title':
+      return renderTitle(deserialized, field);
+    default:
+      return 'default';
+  }
+};
+
+const renderTitle = (deserialized, field) => {
+  // TODO: change the render with EditorState --> at the moment deserialize makes no sense
+  const content = deserialized[field].getCurrentContent();
+  const raw = convertToRaw(content);
+  const firstBlock = raw.blocks[0];
+  return firstBlock.text.length > 0 ? firstBlock.text : 'Untitled document';
+};
