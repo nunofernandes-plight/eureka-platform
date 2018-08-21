873d330caf06dc38bbd81fd2c64dcd930d0ee947
lucaspelloni2 <lucas_pelloni@hotmail.com>
2018-08-21 18:35:58 +0200

    NavPills routes

---
+++ src/frontend/webpack/components/routers/NavPillRoutes.js
@@ -0,0 +1,17 @@
+export const NavPillRoutes = [
+    {
+        path: 'drafts',
+        icon: 'material',
+        material: 'gesture'
+    },
+    {
+        path: 'submitted',
+        icon: 'material',
+        material: 'gesture'
+    },
+    {
+        path: 'review',
+        icon: 'material',
+        material: 'rate_review'
+    }
+];
