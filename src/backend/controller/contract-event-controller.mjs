3fdeada1a03302d3c6f76c1b789d74ab3e3073b7
Andy <andreas.schaufelbuehl@gmail.com>
2018-08-23 10:30:54 +0200

    route & service for adding editor-approved review into DB

--- src/backend/controller/contract-event-controller.mjs
+++ src/backend/controller/contract-event-controller.mjs
@@ -160,6 +160,8 @@ export default {
       undefined,
       async (error, event) => {
         if (error) throw error;
+
+
         console.log('REVIEW ADDED!!!');
       }
     );
--- src/backend/db/review-service.mjs
+++ src/backend/db/review-service.mjs
@@ -8,14 +8,30 @@ export default {
   createReview: async (submissionId, articleHash, stateTimestamp) => {
     const review = new Review({submissionId, articleHash, stateTimestamp});
     return review.save(err => {
-      if (err) return console.error(err);
+      if (err) throw err;
       console.log('Created new review on DB done');
     });
   },
   getReviewById: async (userAddress, reviewId) => {
     const review = await Review.findById(reviewId);
-    if(!review) errorThrower.noEntryFoundById(reviewId);
-    if(review.reviewerAddress !== userAddress) errorThrower.notCorrectEthereumAddress();
+    if (!review) errorThrower.noEntryFoundById(reviewId);
+    if (review.reviewerAddress !== userAddress) errorThrower.notCorrectEthereumAddress();
     return review;
+  },
+
+  addEditorApprovedReview: async (userAddress, reviewId, reviewText, reviewHash,
+    score1, score2, articleHasMajorIssues, articleHasMinorIssues) => {
+    const review = await Review.findById(reviewId);
+    if (!review) errorThrower.noEntryFoundById(reviewId);
+    if (review.reviewerAddress !== userAddress) errorThrower.notCorrectEthereumAddress();
+
+    review.reviewHash = reviewHash;
+    review.reviewText = reviewText;
+    review.reviewScore1 = score1;
+    review.reviewScore2 = score2;
+    review.hasMajorIssues= articleHasMajorIssues;
+    review.hasMinorIssues = articleHasMinorIssues;
+    await review.save();
+    return 'Added editor-approved review into DB.';
   }
 };
