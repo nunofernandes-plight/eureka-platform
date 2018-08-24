743a5b5d73ef881fad59dc9342d28000f14f026c
lucaspelloni2 <lucas_pelloni@hotmail.com>
2018-08-24 09:46:48 +0200

    logout post instead of get

--- src/backend/routes/logout-routes.mjs
+++ src/backend/routes/logout-routes.mjs
@@ -2,7 +2,7 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/', function(req, res) {
+router.post('/', function(req, res) {
   req.logout(req.user);
   req.session.destroy();
   res.send('Successfully logged out');
