2d31fc7993e1afb16f4be748a8e4c1d5b6d331fd
lucaspelloni2 <lucas_pelloni@hotmail.com>
2018-08-27 16:30:42 +0200

    card fixed

--- src/frontend/webpack/components/ContractOwnerDashboard.js
+++ src/frontend/webpack/components/ContractOwnerDashboard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import {Card} from "../views/Card.js";
 
 class ContractOwnerDashboard extends React.Component {
   constructor() {
