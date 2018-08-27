af402b9fb0c5cf790c11c7ce766db07a6b120b23
lucaspelloni2 <lucas_pelloni@hotmail.com>
2018-08-27 15:50:32 +0200

    SmartContract Input data styled

--- src/frontend/webpack/views/SmartContractInputData.js
+++ src/frontend/webpack/views/SmartContractInputData.js
@@ -1,16 +1,54 @@
 import React from 'react';
 import styled from 'styled-components';
+import {__ALERT_ERROR, __FIFTH} from '../../helpers/colors.js';
+
+const Container = styled.div``;
+
+const List = styled.ul`
+  list-style: none;
+  padding-left: 0;
+  border: 1px solid white;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  padding-right: 0;
+  border-radius: 5px;
+  background: ${__FIFTH};
+`;
+
+const Point = styled.li``;
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Title = styled.h4`
+  color: white;
+  margin: 5px 0;
+`;
 
 const SmartContractInputData = props => {
-	return (
-		<div>
-			<ul>
-				<li>Hash: {props.inputData.hash}</li>
-				<li>Authors: {props.inputData.authors}</li>
-				<li>Url: {props.inputData.url}</li>
-			</ul>
-		</div>
-	);
+  return (
+    <Container>
+      <List>
+        <Point>
+          <Content>
+            <Title>Hash</Title> {props.inputData.hash}
+          </Content>
+        </Point>
+        <Point>
+          <Content>
+            <Title>Authors</Title> {props.inputData.authors}
+          </Content>
+        </Point>
+        <Point>
+          <Content>
+            <Title>URL</Title> {props.inputData.url}
+          </Content>
+        </Point>
+      </List>
+    </Container>
+  );
 };
 
 export default SmartContractInputData;
