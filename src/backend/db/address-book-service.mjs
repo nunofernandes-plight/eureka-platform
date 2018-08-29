9371265749ee6208690da415d762bcf5449a8380
severinwullschleger <severin.wullschleger@bluewin.ch>
2018-08-29 15:41:19 +0200

    address book service create and get

---
+++ src/backend/db/address-book-service.mjs
@@ -0,0 +1,29 @@
+import AddressBook from '../schema/address-book.mjs';
+import errorThrower from '../helpers/error-thrower.mjs';
+
+export default {
+  getContacts: async (address) => {
+    const contacts = await AddressBook.find({addressBookOwnerAddress: address});
+    if (!contacts) errorThrower.internalError();
+      return contacts;
+  },
+  createContact: (addressBookOwnerAddress,
+                  {
+                    contactAddress,
+                    preName,
+                    lastName,
+                    info
+                  }) => {
+    const contact = new AddressBook({
+      addressBookOwnerAddress: addressBookOwnerAddress,
+      contactAddress: contactAddress,
+      preName: preName,
+      lastName: lastName,
+      info: info
+    });
+    return contact.save(function(err) {
+      if (err) return console.error(err);
+      console.log('Created new contact on DB done');
+    });
+  }
+};
