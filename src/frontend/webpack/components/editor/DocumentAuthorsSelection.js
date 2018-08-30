9e3886b68643d798072635f5c62efe5fd02216da
lucaspelloni2 <lucas_pelloni@hotmail.com>
2018-08-30 08:48:15 +0200

    Search selecetion on edit authors modal

--- src/frontend/webpack/components/editor/DocumentAuthorsSelection.js
+++ src/frontend/webpack/components/editor/DocumentAuthorsSelection.js
@@ -2,22 +2,68 @@ import React from 'react';
 import styled from 'styled-components';
 import {InputField} from '../../design-components/Inputs.js';
 import {getDomain} from '../../../../helpers/getDomain.js';
-import Roles from '../../../../backend/schema/roles-enum.mjs';
+import Avatar from '../../views/Avatar.js';
+import {__GRAY_100, __GRAY_200, __GRAY_800} from '../../../helpers/colors.js';
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
+  width: 650px;
+  min-height: 150px;
 `;
 
-const Users = styled.div`
+const Users = styled.ul`
+  padding-left: 0;
+  list-style: none;
+  margin: 0;
+  border: 1px solid ${__GRAY_100};
+  max-height: 180px;
+  overflow: scroll;
+`;
+
+const User = styled.li`
+  &:hover {
+    background: ${__GRAY_200};
+  }
+  transition: 0.3s all;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  padding: 10px;
+`;
+
+const AuthorsSection = styled.div`
+  border: 1px solid ${__GRAY_100};
+  border-radius: 4px;
+  width: 100%;
+  margin-bottom: 20px;
+`;
+
+const Title = styled.h4`
+  margin-top: 0;
+  margin-bottom: 0;
+  text-align: left;
+  padding: 12px;
+  font-weight: normal;
+`;
+
+const Authors = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: flex-start;
+  padding: 12px;
 `;
 
-const User = styled.div`
-  display: flex;
+const Author = styled.div``;
+
+const Email = styled.div``;
+
+const AuthorContainer = styled.div``;
+const TopContainer = styled.div`
+  background: ${__GRAY_200};
 `;
 
+const SearchSection = styled.div``;
 class DocumentAuthorsSelection extends React.Component {
   constructor() {
     super();
@@ -54,17 +100,56 @@ class DocumentAuthorsSelection extends React.Component {
   render() {
     return (
       <Container>
-        <InputField
-          placeholder={'Search for an email in our system'}
-          onChange={e => this.handleInput(e.target.value)}
-        />
-        {!this.state.users ? null : (
-          <Users>
-            {this.state.users.map((user, index) => {
-              return <User key={index}>{user.email}</User>;
+        <AuthorsSection>
+          <TopContainer>
+            <Title>Authors</Title>
+          </TopContainer>
+          <Authors>
+            {this.props.document.authors.map((author, index) => {
+              return (
+                <AuthorContainer>
+                  <Author key={index}>{author}</Author>
+                </AuthorContainer>
+              );
             })}
-          </Users>
-        )}
+          </Authors>
+        </AuthorsSection>
+
+        <SearchSection>
+          <InputField
+            placeholder={'Search for an email in our system'}
+            onChange={e => this.handleInput(e.target.value)}
+          />
+          {!this.state.users ? null : (
+            <Users>
+              {this.state.users.map((user, index) => {
+                return (
+                  <User
+                    key={index}
+                    onClick={() => {
+                      const authors = this.props.document.authors;
+                      authors.push(user.ethereumAddress);
+                      this.props.updateDocument({
+                        document: {
+                          ...this.props.document,
+                          authors
+                        }
+                      });
+                    }}
+                  >
+                    <Avatar
+                      avatar={user.avatar}
+                      width={28}
+                      height={28}
+                      right={10}
+                    />
+                    <Email>{user.email}</Email>
+                  </User>
+                );
+              })}
+            </Users>
+          )}
+        </SearchSection>
       </Container>
     );
   }
