b0b05574d8fa30fcf0d538f77ce19490f178d599
severinwullschleger <severin.wullschleger@bluewin.ch>
2018-08-31 10:57:29 +0200

    address book row styling and extending

--- src/frontend/webpack/components/addressbook/AddressBookTableRow.js
+++ src/frontend/webpack/components/addressbook/AddressBookTableRow.js
@@ -6,13 +6,14 @@ import {
   __GRAY_600,
   __GRAY_200,
   __THIRD,
-  __FIFTH, __MAIN, __ALERT_SUCCESS
+  __FIFTH, __MAIN, __ALERT_SUCCESS, __GRAY_300
 } from '../../../helpers/colors.js';
 import {renderField} from '../editor/DocumentRenderer.js';
 import {renderTimestamp} from '../../../helpers/timestampRenderer.js';
 import {MEDIUM_DEVICES} from '../../../helpers/mobile.js';
 import Icon from '../../views/icons/Icon.js';
 import {InputField} from '../../design-components/Inputs.js';
+import {createContact, getContacts} from './AddressBookMethods.js';
 
 const Tr = styled.tr`
   &:hover {
@@ -21,95 +22,141 @@ const Tr = styled.tr`
   transition: 0.5s all;
 `;
 
-const AddressBookTableRow = props => {
-  if (props.editable)
+const Td = styled.td`
+  padding: 15px 0;
+`;
+
+const Tick = styled(Icon)`
+  opacity: ${props => (props.valid ? '1' : '0.5')};
+  pointer-events: ${props => (props.valid ? 'auto' : 'none')};
+  background-color: ${props => (props.valid ? __ALERT_SUCCESS : __GRAY_300)};
+`;
+
+class AddressBookTableRow extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      contactAddress: null,
+      firstName: null,
+      lastName: null,
+      comment: null
+    };
+  }
+
+  validate() {
     return (
-      <Tr key={props.contact.contactAddress}>
-        <td>
-          {props.contact.contactAddress}
-        </td>
-        <td>
-          <InputField
-            value={props.contact.preName}
-            placeholder={'First Name'}
-            onChange={(e) =>{props.onChange}}
-          />
-        </td>
-        <td>
-          <InputField
-            value={props.contact.lastName}
-            placeholder={'Last Name'}
-          />
-        </td>
-        <td>
-          <InputField
-            value={props.contact.info}
-            placeholder={'Last Name'}
-          />
-        </td>
-        <td>
-          <Icon
-            icon={'check'}
-            width={20}
-            height={20}
-            color={__ALERT_SUCCESS}
-            onClick={() => {
-              props.onSave(this.state.newContact);
-            }}
-          />
-        </td>
-        <td>
-          <Icon
-            icon={'delete'}
-            width={20}
-            height={20}
-            color={__ALERT_ERROR}
-            onClick={() => {
-              props.onDelete(props.contact.contactAddress);
-            }}
-          />
-        </td>
-      </Tr>
+      this.state.firstName &&
+      this.state.lastName
     );
-  else
-    return (
-      <Tr key={props.contact.contactAddress}>
-        <td>
-          {props.contact.contactAddress}
-        </td>
-        <td>
-          {props.contact.preName}
-        </td>
-        <td>
-          {props.contact.lastName}
-        </td>
-        <td>
-          {props.contact.info}
-        </td>
-        <td>
-          <Icon
-            icon={'edit'}
-            width={20}
-            height={20}
-            color={__MAIN}
-            onClick={() => {
-              props.onEdit(props.contact.contactAddress);
-            }}
-          />
-        </td>
-        <td>
-          <Icon
-            icon={'delete'}
-            width={20}
-            height={20}
-            color={__ALERT_ERROR}
-            onClick={() => {
-              props.onDelete(props.contact.contactAddress);
-            }}
-          />
-        </td>
-      </Tr>
-    );
-};
+  }
+
+  handleInput(stateKey, e) {
+    this.setState({[stateKey]: e.target.value});
+  }
+
+  componentDidMount() {
+    this.setState({
+      contactAddress: this.props.contact.contactAddress,
+      firstName: this.props.contact.preName,
+      lastName: this.props.contact.lastName,
+      comment: this.props.contact.info
+    })
+  }
+
+  render() {
+    if (this.props.editable)
+      return (
+        <Tr key={this.state.contactAddress}>
+          <Td>
+            {this.state.contactAddress}
+          </Td>
+          <td>
+            <InputField
+              value={this.state.firstName}
+              placeholder={'First Name'}
+              onChange={e => this.handleInput('firstName', e)}
+            />
+          </td>
+          <td>
+            <InputField
+              value={this.state.lastName}
+              placeholder={'Last Name'}
+              onChange={e => this.handleInput('lastName', e)}
+            />
+          </td>
+          <td>
+            <InputField
+              value={this.state.comment}
+              placeholder={'Comment'}
+              onChange={e => this.handleInput('comment', e)}
+            />
+          </td>
+          <td>
+            <Tick
+              valid={this.validate()}
+              icon={'check'}
+              width={20}
+              height={20}
+              color={__ALERT_SUCCESS}
+              onClick={() => {
+                this.props.onSave(this.state.contactAddress, this.state.firstName, this.state.lastName, this.state.comment);
+              }}
+            />
+          </td>
+          <td>
+            <Icon
+              icon={'delete'}
+              width={20}
+              height={20}
+              color={__ALERT_ERROR}
+              onClick={() => {
+                this.props.onDelete(this.props.contact.contactAddress);
+              }}
+            />
+          </td>
+        </Tr>
+      );
+    else
+      return (
+        <Tr key={this.state.contactAddress}>
+          <Td>
+            {this.state.contactAddress}
+          </Td>
+          <td>
+            {this.state.firstName}
+          </td>
+          <td>
+            {this.state.lastName}
+          </td>
+          <td>
+            {this.state.comment}
+          </td>
+          <td>
+            <Icon
+              valid={this.validate()}
+              icon={'edit'}
+              width={20}
+              height={20}
+              color={__MAIN}
+              onClick={() => {
+                this.props.onEdit(this.state.contactAddress);
+              }}
+            />
+          </td>
+          <td>
+            <Icon
+              icon={'delete'}
+              width={20}
+              height={20}
+              color={__ALERT_ERROR}
+              onClick={() => {
+                this.props.onDelete(this.state.contactAddress);
+              }}
+            />
+          </td>
+        </Tr>
+      );
+  };
+}
 
 export default AddressBookTableRow;
