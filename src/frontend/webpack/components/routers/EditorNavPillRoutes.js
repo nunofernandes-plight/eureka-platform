c4124b5e86a4d7dfaba9c60224c0d5612d6ba3cc
severinwullschleger <severin.wullschleger@bluewin.ch>
2018-09-05 11:21:18 +0200

    added editor router

---
+++ src/frontend/webpack/components/routers/EditorNavPillRoutes.js
@@ -0,0 +1,26 @@
+export const EditorNavPillRoutes = [
+  {
+    path: 'signoff',
+    name: 'Sign Off',
+    icon: 'material',
+    material: 'gesture'
+  },
+  {
+    path: 'invite',
+    name: 'Invite Reviewers',
+    icon: 'material',
+    material: 'gesture'
+  },
+  {
+    path: 'reviews',
+    name: 'Check Reviews',
+    icon: 'material',
+    material: 'gesture'
+  },
+  {
+    path: 'finalize',
+    name: 'Finalize',
+    icon: 'material',
+    material: 'gesture'
+  }
+];
