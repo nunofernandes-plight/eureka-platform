6a6e773a8e975886df3c1500a29a4272d2a80bc6
lucaspelloni2 <lucas_pelloni@hotmail.com>
2018-09-12 16:13:45 +0200

    get submissions from backend

--- src/frontend/webpack/components/Editor/EditorArticles.js
+++ src/frontend/webpack/components/Editor/EditorArticles.js
@@ -2,8 +2,16 @@ import React from 'react';
 import styled from 'styled-components';
 import {Card} from '../../views/Card.js';
 import EditorQuerySection from './EditorQuerySection.js';
+import {Table} from '../../design-components/Table/Table.js';
+import {getUnassignedSubmissions} from './EditorMethods.js';
+import Modal from '../../design-components/Modal.js';
 
 const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Articles = styled.div`
   display: flex;
   flex-direction: column;
 `;
@@ -14,7 +22,8 @@ class EditorArticles extends React.Component {
     this.state = {
       query: null,
       articles: null,
-      filtersActive: false
+      filtersActive: false,
+      loading: false
     };
   }
 
@@ -22,11 +31,48 @@ class EditorArticles extends React.Component {
     this.setState({[field]: value});
   };
 
-  componentDidMount() {}
+  componentDidMount() {
+    this.setState({loading: true});
+    getUnassignedSubmissions()
+      .then(response => response.json())
+      .then(response => {
+        if (response.success) {
+          this.setState({articles: response.data});
+        } else {
+          this.setState({
+            errorMessage: response.error,
+            loading: false
+          });
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        this.setState({
+          errorMessage: 'Ouh. Something went wrong.',
+          loading: false
+        });
+      });
+  }
+
+  renderModals() {
+    return (
+      <Modal
+        type={'notification'}
+        toggle={isErrorMessage => {
+          this.setState({errorMessage: null});
+        }}
+        show={this.state.errorMessage}
+        title={'You got the following error'}
+      >
+        {this.state.errorMessage}
+      </Modal>
+    );
+  }
 
   render() {
     return (
       <Container>
+        {this.renderModals()}
         <Card width={1000} title={'Allocate articles'}>
           <EditorQuerySection
             checked={this.state.filtersActive}
@@ -37,6 +83,8 @@ class EditorArticles extends React.Component {
               this.handleQuery(field, value);
             }}
           />
+
+          <Articles />
         </Card>
       </Container>
     );
---
+++ src/frontend/webpack/views/Article.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const FigureContainer = styled.div``;
+const Figure = styled.img``;
+
+const Title = styled.h4``;
+const Article = props => {
+  return (
+    <Container>
+      <Title>asfoaojsfjoafjos</Title>
+    </Container>
+  );
+};
+
+export default Article;
