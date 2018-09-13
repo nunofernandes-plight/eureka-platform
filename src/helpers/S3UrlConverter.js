5f000b15d88b20e0c16eda17a224f615623f4239
lucaspelloni2 <lucas_pelloni@hotmail.com>
2018-09-13 10:53:22 +0200

    S3Url converter for queries and urls

--- src/helpers/S3UrlConverter.js
+++ src/helpers/S3UrlConverter.js
@@ -1,3 +1,5 @@
-export const fromS3toCdn = url => {
-	return 'https://cdn.sciencematters.io' + url.toString().slice(36) + '?max-w=700&fm=png';
+export const fromS3toCdn = (url, query) => {
+  return (
+    'https://cdn.sciencematters.io' + url.toString().slice(36) + '?' + query
+  );
 };
