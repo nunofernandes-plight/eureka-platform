54fa71e471a3c8d43c8c23315d38ac6ee0bca37e
lucaspelloni2 <lucas_pelloni@hotmail.com>
2018-09-13 14:25:11 +0200

    truncate text on abstract + css transform

--- src/frontend/webpack/views/ArticleCard.js
+++ src/frontend/webpack/views/ArticleCard.js
@@ -1,26 +1,27 @@
 import React from 'react';
 import styled from 'styled-components';
 import {
+  __ALERT_DANGER,
+  __ALERT_WARNING,
   __GRAY_100,
-  __GRAY_200,
-  __GRAY_400,
-  __THIRD
+  __GRAY_200
 } from '../../helpers/colors.js';
 import {fromS3toCdn} from '../../../helpers/S3UrlConverter.js';
 import {LARGE_DEVICES} from '../../helpers/mobile.js';
 import AuthorLookup from '../components/AuthorLookup.js';
 import {renderField} from '../components/TextEditor/DocumentRenderer.js';
+import TextTruncate from 'react-text-truncate';
 
 const Container = styled.div`
   display: flex;
-  border: 1px solid ${__GRAY_200};
   margin: 25px 0;
   width: 100%;
   min-height: 350px;
   &:hover {
     box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
+    transform: scale(1.02);
   }
-  transition: 0.25s all;
+  transition: all 0.2s ease-in-out;
   background: ${__GRAY_100};
 `;
 
@@ -52,7 +53,9 @@ const TitleSection = styled.div`
   display: flex;
   flex-direction: column;
   width: 35%;
-  padding: 0 10px;
+  padding: 1em;
+  font-family: 'Roboto', sans-serif;
+  margin-left: 1em;
 `;
 const getFigureLink = (url, width, height) => {
   return fromS3toCdn(url, `fit=crop&w=${width}&h=${height}&auto=compress`);
@@ -66,9 +69,19 @@ const Authors = styled.div`
   margin-bottom: auto;
 `;
 
+const Abstract = styled.p`
+  margin-top: 0;
+  font-style: italic;
+`;
+
 const MyLabel = styled.label`
   font-size: 12px;
-  font-weight: 100;
+  font-weight: bold;
+`;
+const ReadMore = styled.a`
+  color: ${__ALERT_DANGER};
+  font-style: italic;
+  font-weight: bold;
 `;
 const ArticleCard = ({article}) => {
   console.log(article);
@@ -87,7 +100,7 @@ const ArticleCard = ({article}) => {
             right={10}
             width={35}
             height={35}
-            padding={'5px'}
+            padding={'12px'}
           />
         </Authors>
       </FigureSection>
@@ -95,6 +108,25 @@ const ArticleCard = ({article}) => {
       <TitleSection>
         <MyLabel>Title</MyLabel>
         <Title>{renderField(article, 'title')}</Title>
+
+        <MyLabel style={{marginTop: '25px'}}>Abstract</MyLabel>
+        <Abstract>
+          <TextTruncate
+            text={
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean\n' +
+              '          feugiat orci vitae leo maximus malesuada. Suspendisse suscipit nulla\n' +
+              '          non augue ultricies, eu eleifend felis volutpat. Maecenas venenatis,\n' +
+              '          erat nec viverra mattis, elit libero tristique massa, a imperdiet nibh\n' +
+              '          ligula id urna. Morbi ullamcorper sodales semper. Fusce dignissim,\n' +
+              '          erat sed ornare viverra, elit lacus faucibus tellus, nec varius odio\n' +
+              '          enim eget risus. Nullam eu ipsum ex. Suspendisse sed dapibus ante.\n' +
+              '          Proin placerat urna in mauris vehicula imperdiet.'
+            }
+            line={6}
+            truncateText={'...'}
+            textTruncateChild={<ReadMore href="#">Read More</ReadMore>}
+          />
+        </Abstract>
       </TitleSection>
       <FieldsSection />
     </Container>
