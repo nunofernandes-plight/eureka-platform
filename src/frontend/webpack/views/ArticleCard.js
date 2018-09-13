daf764788b14134cbb4ca423c0dc60c2fab71aa9
lucaspelloni2 <lucas_pelloni@hotmail.com>
2018-09-13 11:17:50 +0200

    img layout fixed for articles

--- src/frontend/webpack/views/ArticleCard.js
+++ src/frontend/webpack/views/ArticleCard.js
@@ -2,19 +2,30 @@ import React from 'react';
 import styled from 'styled-components';
 import {__THIRD} from '../../helpers/colors.js';
 import {fromS3toCdn} from '../../../helpers/S3UrlConverter.js';
+import Author from './Author.js';
+import {LARGE_DEVICES} from '../../helpers/mobile.js';
 
 const Container = styled.div`
   display: flex;
   border: 1px solid ${__THIRD};
-  margin: 15px 10px;
+  margin: 25px 0;
+  width: 100%;
+  min-height: 300px;
 `;
 
 const FigureSection = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 40%;
 `;
 
-const Figure = styled.img``;
+const Figure = styled.img`
+  ${LARGE_DEVICES`
+  width:100%; 
+  `};
+`;
 
 const Figures = styled.div`
   display: flex;
@@ -32,19 +43,26 @@ const TitleSection = styled.div`
 const getFigureLink = (url, width, height) => {
   return fromS3toCdn(url, `fit=crop&w=${width}&h=${height}&auto=compress`);
 };
+
+const Authors = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 const ArticleCard = ({article}) => {
-  if (article.figure.length > 0) {
-    const url = article.figure[0].url;
-    /*    console.log(getFigureLink(url, 760, 420));*/
-  }
   return (
     <Container>
       <FigureSection>
         {article.figure.length === 0 ? (
           <Figure src="/img/noPicture.png" width={'auto'} height={140} />
         ) : (
-          <Figure src={getFigureLink(article.figure[0].url, 400, 250)} />
+          <Figure src={getFigureLink(article.figure[0].url, 350, 250)} />
         )}
+
+        <Authors>
+          {article.authors.map((a, i) => {
+            return <Author key={i} author={a} />;
+          })}
+        </Authors>
       </FigureSection>
 
       <TitleSection />
