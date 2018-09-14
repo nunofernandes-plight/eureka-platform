e15e5c76a99c59cc00cc4201c09826acdf9adeff
severinwullschleger <severin.wullschleger@bluewin.ch>
2018-09-14 17:37:04 +0200

    added contract owner service

---
+++ src/backend/db/contract-owner-service.mjs
@@ -0,0 +1,17 @@
+import {getContractOwner} from '../web3/web3-platform-contract-methods.mjs';
+import ContractOwner from '../schema/contract-owner.mjs';
+
+export const writeContractOwnerInDB = async contract => {
+  const id = 1;
+  const contractOwnerAddress = await getContractOwner(contract);
+  let contractOwner = await ContractOwner.findById(id);
+  if(!contractOwner) {
+    contractOwner = new ContractOwner({
+      _id: id,
+      address: contractOwnerAddress
+    });
+  } else {
+    contractOwner.address = contractOwnerAddress;
+  }
+  return contractOwner.save();
+};
