2c879a7a827b18b8ef393e03bfbed31aaede6d6c
severinwullschleger <severin.wullschleger@bluewin.ch>
2018-09-18 11:01:49 +0200

    implemented article getters in frontend

--- src/frontend/webpack/components/Editor/EditorMethods.js
+++ src/frontend/webpack/components/Editor/EditorMethods.js
@@ -9,3 +9,43 @@ export const getUnassignedSubmissions = () => {
     credentials: 'include'
   });
 };
+
+export const getArticlesToSignOff = () => {
+  return fetch(`${getDomain()}/api/articles/assigned/signoff`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    credentials: 'include'
+  });
+};
+
+export const getInviteReviewersArticles = () => {
+  return fetch(`${getDomain()}/api/articles/assigned/inviteReviewers`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    credentials: 'include'
+  });
+};
+
+export const getCheckReviewersArticles = () => {
+  return fetch(`${getDomain()}/api/articles/assigned/checkReviews`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    credentials: 'include'
+  });
+};
+
+export const getArticlesToFinalize = () => {
+  return fetch(`${getDomain()}/api/articles/assigned/finalize`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    credentials: 'include'
+  });
+};
