190602a0dba1cb04e82d34a15a681faf7566568c
lucaspelloni2 <lucas_pelloni@hotmail.com>
2018-09-28 11:08:39 +0200

    sha256 with canon

--- src/helpers/getHexAndHash.mjs
+++ src/helpers/getHexAndHash.mjs
@@ -1,8 +1,9 @@
 import sha256 from 'js-sha256';
-import { stringify } from 'canonicaljson';
+import CANON from 'canon';
 
 export const getArticleHashFromDocument = document => {
-  return sha256(stringify(document));
+  console.log(document);
+  return sha256(CANON.stringify(document));
 };
 
 export const getArticleHexFromDocument = document => {};
