be1d3eb7cedc5cbef11d311a0559454bd46a589a
lucaspelloni2 <lucas_pelloni@hotmail.com>
2018-09-29 16:31:30 +0200

    get hash with canon

--- src/helpers/getHexAndHash.mjs
+++ src/helpers/getHexAndHash.mjs
@@ -1,9 +1,20 @@
 import sha256 from 'js-sha256';
 import CANON from 'canon';
+import {deserializeDocument} from './documentSerializer.mjs';
+import Document from '../models/Document.mjs';
+import {renderField} from '../frontend/webpack/components/TextEditor/DocumentRenderer.mjs';
 
 export const getArticleHashFromDocument = document => {
-  console.log(document);
-  return sha256(CANON.stringify(document));
+  const doc = new Document(document);
+  let articleHash = '';
+  doc.getAllFields().map(field => {
+    articleHash += hashField(field);
+  });
+  return sha256(CANON.stringify(articleHash));
+};
+
+export const hashField = field => {
+  return sha256(CANON.stringify(field));
 };
 
 export const getArticleHexFromDocument = document => {};
