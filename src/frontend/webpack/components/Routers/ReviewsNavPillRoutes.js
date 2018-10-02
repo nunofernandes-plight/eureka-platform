43227253968cfb29e9bbf530cf8519bbacc1f1c0
severinwullschleger <severin.wullschleger@bluewin.ch>
2018-10-02 12:21:25 +0200

    review nav pills #52

---
+++ src/frontend/webpack/components/Routers/ReviewsNavPillRoutes.js
@@ -0,0 +1,20 @@
+export const ReviewsNavPillRoutes = [
+  {
+    path: 'invited',
+    name: 'Review Invitations',
+    icon: 'material',
+    material: 'mail'
+  },
+  {
+    path: 'open',
+    name: 'Articles Open to Review',
+    icon: 'material',
+    material: 'search'
+  },
+  {
+    path: 'myreviews',
+    name: 'My Reviews',
+    icon: 'material',
+    material: 'rate_review'
+  }
+];
