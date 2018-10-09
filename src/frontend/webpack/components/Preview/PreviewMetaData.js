3f8562a117d8afb38bc1ed95cc43f23f938e99ba
lucaspelloni2 <lucas_pelloni@hotmail.com>
2018-10-09 13:45:09 +0200

    PreviewMetadata initialized

--- src/frontend/webpack/components/Preview/PreviewMetaData.js
+++ src/frontend/webpack/components/Preview/PreviewMetaData.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
+import Document from '../../../../models/Document.mjs';
+import {__ALERT_ERROR, __GRAY_800} from '../../../helpers/colors.js';
 
 const Container = styled.div`
   display: flex;
@@ -7,8 +9,46 @@ const Container = styled.div`
   flex: 1 1 0;
 `;
 
-const PreviewMetaData = props => {
-  return <Container>PreviewMetaData</Container>;
+const FieldContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Title = styled.h4`
+  color: ${__ALERT_ERROR};
+  font-size: 12.5px;
+  font-weight: bold;
+  margin-bottom: 0;
+  margin-top: 0;
+`;
+
+const Content = styled.div`
+  color: ${__GRAY_800};
+  font-size: 10px;
+  font-style: italic;
+`;
+
+const Field = ({doc, field, ...otherProps}) => {
+  let content = doc[field];
+  console.log(content);
+  if (!content || content === '') content = '-';
+  return (
+    <FieldContainer>
+      <Title>{field}</Title>
+{/*      <Content>{content}</Content>*/}
+    </FieldContainer>
+  );
+};
+
+const PreviewMetaData = ({document, ...otherProps}) => {
+  const fields = Document.metaDataFields();
+  return (
+    <Container>
+      {fields.map((field, i) => {
+        return <Field key={i} doc={document} field={field} />;
+      })}
+    </Container>
+  );
 };
 
 export default PreviewMetaData;
