2aa391d41501bd1992737a526abe1046caaaf368
lucaspelloni2 <lucas_pelloni@hotmail.com>
2018-10-11 16:35:21 +0200

    ReviewsWriter first draf

--- src/frontend/webpack/components/Reviews/ReviewsWriter.js
+++ src/frontend/webpack/components/Reviews/ReviewsWriter.js
@@ -1,18 +1,127 @@
 import React from 'react';
 import styled from 'styled-components';
+import {fetchArticle} from '../TextEditor/DocumentMainMethods.js';
+import Document from '../../../../models/Document.mjs';
+import {deserializeDocument} from '../../../../helpers/documentSerializer.mjs';
+import withRouter from 'react-router/es/withRouter.js';
+import Modal from '../../design-components/Modal.js';
+import {Card} from '../../views/Card.js';
+import {Go} from '../Routers/Go.js';
+import GridSpinner from '../../views/spinners/GridSpinner.js';
+import {renderField} from '../TextEditor/DocumentRenderer.mjs';
+import PreviewStatus from '../../views/PreviewStatus.js';
+import {__GRAY_100} from '../../../helpers/colors.js';
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.h3`
+  font-size: 26px;
+  font-weight: bold;
+  line-height: 1.3;
+  font-family: 'Roboto', sans-serif;
+  margin-bottom: 10px;
+  margin-top: 0;
+`;
+
+const MySeparator = styled.div`
+  height: 2px;
+  display: flex;
+  width: 75%;
+  background: ${__GRAY_100};
+  justify-content: center;
+  margin-top: 25px;
+`;
+
+const MyPreview = styled.div`
+  display: flex;
+  padding: 20px;
+  width: 100%;
+`;
+
+const ArticlePreview = styled.div`
+  flex: 6.5 1 0;
+`;
 
 class ReviewsWriter extends React.Component {
   constructor() {
     super();
+    this.state = {
+      errorMessage: null
+    };
+  }
+
+  componentDidMount() {
+    const draftId = this.props.match.params.id;
+    fetchArticle(draftId)
+      .then(response => response.json())
+      .then(response => {
+        if (response.success) {
+          let document = new Document(response.data.document);
+          let deserialized = deserializeDocument(document);
+          this.setState({
+            document: deserialized
+          });
+        } else {
+          this.setState({
+            errorMessage: response.error
+          });
+        }
+        this.setState({loading: false});
+      })
+      .catch(err => {
+        console.log(err);
+        this.setState({
+          errorMessage: 'Ouh. Something went wrong.',
+          loading: false
+        });
+      });
+  }
+
+  renderModal() {
+    return (
+      <div>
+        {' '}
+        <Modal
+          type={'notification'}
+          toggle={isErrorMessage => {
+            this.setState({errorMessage: null});
+          }}
+          show={this.state.errorMessage}
+          title={'You got the following error'}
+        >
+          {this.state.errorMessage}
+        </Modal>
+      </div>
+    );
   }
 
   render() {
     return (
-      <Container>asfoahsofhsafoasoihfiahosfhioasfhiasfhioashiof</Container>
+      <Container>
+        {this.renderModal()}
+        <Card title={'Write Your Review'}>
+          <Go back {...this.props} />
+          <MySeparator />
+          {!this.state.document ? (
+            <GridSpinner />
+          ) : (
+            <MyPreview>
+              {' '}
+              <ArticlePreview>
+                <Title>{renderField(this.state.document, 'title')}</Title>
+                <PreviewStatus status={this.state.document.state} />
+              </ArticlePreview>
+            </MyPreview>
+          )}
+        </Card>
+      </Container>
     );
   }
 }
 
-export default ReviewsWriter;
+export default withRouter(ReviewsWriter);
