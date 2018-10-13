f1bf87555ee34201ecd6abe97189308ae5e265cf
lucaspelloni2 <lucas_pelloni@hotmail.com>
2018-10-13 23:45:10 +0200

    isReview

--- src/frontend/webpack/components/Preview/PreviewArticleAbstract.js
+++ src/frontend/webpack/components/Preview/PreviewArticleAbstract.js
@@ -18,20 +18,13 @@ const PreviewArticleAbstract = ({document, isReview, ...otherProps}) => {
   abstract =
     'African sleeping sickness is a tropical disease caused by Trypanosoma brucei gambiense or T. b. rhodesiense. Both subspecies are transmitted by the tsetse fly. In general, an infection is lethal without an effective treatment. We used a rodent efficacy model to test 4 compounds that had been previously identified in a novel in vitro screen as activators of parasite differentiation from bloodstream towards procyclic forms. The 4 compounds were trypanocidal in vitro. However, none of the compounds showed trypanocidal activity in vivo. Snapshot pharmacokinetic (PK) profiles indicated that the compound exposure was too low after intraperitoneal administration, which explains the lack of efficacy.';
 
-  return (
-    <Container id={'abstract'}>
+  return <Container id={'abstract'}>
       <PreviewArticleTitleByField field={'abstract'} />
 
       <ReviewsWriterFieldContainer>
         <Abstract>{abstract}</Abstract>
-        {isReview ? (
-          <ReviewsWriterContainer
-            onMouseEnter={() => {}}
-            onMouseLeave={() => {}}
-          />
-        ) : null}
+        {isReview ? <ReviewsWriterContainer onMouseEnter={() => {}} onMouseLeave={() => {}} /> : null}
       </ReviewsWriterFieldContainer>
-    </Container>
-  );
+    </Container>;
 };
 export default PreviewArticleAbstract;
