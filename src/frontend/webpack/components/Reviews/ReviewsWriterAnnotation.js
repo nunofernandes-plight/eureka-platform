520b42a166502b25edd57de066f3eddcf4834ce6
lucaspelloni2 <lucas_pelloni@hotmail.com>
2018-10-15 15:03:50 +0200

    Annotation Header

--- src/frontend/webpack/components/Reviews/ReviewsWriterAnnotation.js
+++ src/frontend/webpack/components/Reviews/ReviewsWriterAnnotation.js
@@ -1,19 +1,42 @@
 import React from 'react';
 import styled from 'styled-components';
+import queryString from 'query-string';
+import {getDomain} from '../../../../helpers/getDomain.mjs';
+import AuthorLookup from '../AuthorLookup.js';
 
-const Container = styled.div``;
+const Container = styled.div`
+  padding: 10px;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.3);
+  border: none;
+`;
+
+const AnnotationHeader = styled.div`
+  display: flex;
+`;
 
 class ReviewsWriterAnnotation extends React.Component {
   constructor() {
     super();
+    this.state = {};
   }
 
-  componentDidMount() {}
+  async componentDidMount() {}
 
   render() {
+    const annotation = this.props.annotation;
     return (
       <Container>
-        <textarea />
+        <AnnotationHeader>
+          <AuthorLookup
+            addresses={annotation.owner}
+            right={5}
+            width={23}
+            height={23}
+            noAddress
+            padding={'5px'}
+          />
+        </AnnotationHeader>
+        <AnnotationBody>{annotation.}</AnnotationBody>
       </Container>
     );
   }
