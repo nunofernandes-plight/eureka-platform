31c5173facd35728bb50a325c492f2415fd6ad8a
lucaspelloni2 <lucas_pelloni@hotmail.com>
2018-10-15 12:46:32 +0200

    ReviewsWriterTextArea

---
+++ src/frontend/webpack/components/Reviews/ReviewsWriterTextAreaContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const ReviewsWriterTextAreaContainer = () => {
+  return <Container>asfasf</Container>;
+};
+
+export default ReviewsWriterTextAreaContainer;
