43d66e797bc1fcce8c92a9c11628d74c07456b43
severinwullschleger <severin.wullschleger@bluewin.ch>
2018-10-16 09:56:38 +0200

    added annotation-routes

---
+++ src/backend/routes/annotation-routes.mjs
@@ -0,0 +1,52 @@
+import express from 'express';
+import {asyncHandler} from '../api/requestHandler.mjs';
+import accesController from '../controller/acess-controller.mjs';
+import annotationService from '../db/annotation-service.mjs';
+const router = express.Router();
+
+router.use(accesController.loggedInOnly);
+
+router.get(
+  '/:articleVersionId/:reviewId',
+  asyncHandler(async req => {
+    let annotations = await annotationService.getAnnotations(req.session.passport.user.ethereumAddress, req.params.articleVersionId);
+    return annotations;
+  })
+);
+
+router.get(
+  '/:articleVersionId',
+  asyncHandler(async req => {
+    let annotations = await annotationService.getAllAnnotations(req.session.passport.user.ethereumAddress, req.params.articleVersionId);
+    return annotations;
+  })
+);
+
+router.post(
+  '/',
+  asyncHandler(async req => {
+    let annotation = await annotationService.createAnnotation(
+      req.session.passport.user.ethereumAddress,
+      req.body);
+    return annotation;
+  })
+);
+
+router.put(
+  '/:annotationId',
+  asyncHandler(async req => {
+    let annotation = await annotationService.editAnnotation(
+      req.session.passport.user.ethereumAddress,
+      req.body);
+    return annotation;
+  })
+);
+
+router.delete(
+  '/:annotationId',
+  asyncHandler(async req => {
+    let annotation = await annotationService.deleteAnnotation(
+      req.session.passport.user.ethereumAddress);
+    return annotation;
+  })
+);
