f8bca0b32e6cd1fd538ba70c7ca1a7e9e8686064
lucaspelloni2 <lucas_pelloni@hotmail.com>
2018-10-23 10:11:20 +0200

    computePagination algorithm

--- src/frontend/webpack/components/Editor/Pagination.js
+++ src/frontend/webpack/components/Editor/Pagination.js
@@ -111,6 +111,65 @@ const NextButton = props => {
   return null;
 };
 
+const Page = ({index, currentPage, totalPages, ...otherProps}) => {
+  return (
+    <Element
+      currentPage={currentPage}
+      key={index}
+      number={index}
+      goToPage={number => {
+        otherProps.goToPage(number);
+      }}
+    >
+      {index}
+    </Element>
+  );
+};
+
+const Dots = ({index, currentPage, ...otherProps}) => {
+  return (
+    <Element
+      index={index}
+      currentPage={currentPage}
+      {...otherProps}
+      goToPage={number => {}}
+    >
+      ...
+    </Element>
+  );
+};
+
+const computePagination = (currentPage, totalPages) => {
+  let current = currentPage,
+    last = totalPages,
+    delta = 1,
+    left = current - delta,
+    right = current + delta + 1,
+    range = [],
+    rangeWithDots = [],
+    l;
+
+  for (let i = 1; i <= last; i++) {
+    if (i === 1 || i === last || (i >= left && i < right)) {
+      range.push(i);
+    }
+  }
+
+  for (let i of range) {
+    if (l) {
+      if (i - l === 2) {
+        rangeWithDots.push(l + 1);
+      } else if (i - l !== 1) {
+        rangeWithDots.push('...');
+      }
+    }
+    rangeWithDots.push(i);
+    l = i;
+  }
+
+  return rangeWithDots;
+};
+
 const Pagination = ({currentPage, totalPages, limit, ...otherProps}) => {
   return (
     <Container>
@@ -125,18 +184,21 @@ const Pagination = ({currentPage, totalPages, limit, ...otherProps}) => {
         .fill(true)
         .map((_, i) => {
           const index = i + 1;
-          return (
-            <Element
-              currentPage={currentPage}
-              key={index}
-              number={index}
-              goToPage={number => {
-                otherProps.goToPage(number);
-              }}
-            >
-              {index}
-            </Element>
-          );
+          const split = computePagination(currentPage, totalPages);
+
+          return split.map(number => {
+            if (number === index) {
+              return (
+                <Page
+                  key={index}
+                  index={index}
+                  currentPage={currentPage}
+                  {...otherProps}
+                  totalPages={totalPages}
+                />
+              );
+            }
+          });
         })}
       <NextButton
         totalPages={totalPages}
