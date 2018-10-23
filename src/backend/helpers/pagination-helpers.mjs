27e3ef307266847ef52fc8144529ae05c5c594a9
severinwullschleger <severin.wullschleger@bluewin.ch>
2018-10-23 09:40:18 +0200

    added pagination helper

---
+++ src/backend/helpers/pagination-helpers.mjs
@@ -0,0 +1,9 @@
+export const getLimitedObjects = (fn, pageNumber, nPerPage) => {
+  return fn
+    .skip(pageNumber > 0 ? (pageNumber - 1) * nPerPage : 0)
+    .limit(nPerPage)
+};
+
+export const getNumberOfObjects = (fn) => {
+  return fn.count();
+};
