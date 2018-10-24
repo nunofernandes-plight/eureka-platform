f71c13e83091cb5fc90696571519bd06b5ebbf18
lucaspelloni2 <lucas_pelloni@hotmail.com>
2018-10-24 16:21:41 +0200

    refs for height in container and children

--- src/frontend/webpack/components/Preview/PreviewArticleAbstract.js
+++ src/frontend/webpack/components/Preview/PreviewArticleAbstract.js
@@ -8,8 +8,6 @@ import {
 } from '../Reviews/Annotations/ReviewsWriterField.js';
 import ReviewsWriterContainer from '../Reviews/Annotations/WriterContainer.js';
 import {tokenizeSentence} from '../Reviews/Annotations/SentenceTokenizer.js';
-
-import ReactDOM from 'react-dom';
 import GridSpinner from '../../views/spinners/GridSpinner.js';
 
 const Container = styled.div``;
@@ -19,52 +17,58 @@ const Abstract = FieldContainer.extend``;
 class PreviewArticleAbstract extends React.Component {
   constructor() {
     super();
+    const abstract =
+      'African sleeping sickness is a tropical disease caused by Trypanosoma brucei gambiense or T. b. rhodesiense. Both subspecies are transmitted by the tsetse fly. In general, an infection is lethal without an effective treatment. We used a rodent efficacy model to test 4 compounds that had been previously identified in a novel in vitro screen as activators of parasite differentiation from bloodstream towards procyclic forms. The 4 compounds were trypanocidal in vitro. However, none of the compounds showed trypanocidal activity in vivo. Snapshot pharmacokinetic (PK) profiles indicated that the compound exposure was too low after intraperitoneal administration, which explains the lack of efficacy. Garden eels live in burrows from which they protrude their bodies to feed on planktonic organisms, show courtship behavior and reproduce, and in which they seek refuge from predators. Despite universal acceptance that garden eels retract into their burrows for predator avoidance, a surprising lack of published accounts of this behaviour exists. Here, opportunist observations made during shark abundance video surveys, show reactions of garden eels during encounters with potential predators and other large-bodied organisms. Brown garden eels (Heteroconger longissimus) were observed during ten encounters with larger fish, and showed variable responses to five different large-bodied species. Varied responses suggested an ability to discriminate between organisms and react according to relative predation risk and proximity. The largest reactions were in response to encounters with piscivorous teleosts, the most likely predators of garden eels. Multiple encounters with two species of sharks, both improbable predators, resulted in a less pronounced reaction, consistent across encounters but variable with proximity. An encounter with a non-predator teleost resulted in the mildest response, despite very close proximity. These observations suggest that garden eels have the ability to discriminate between large-bodied organisms, and react according to relative predation risk.';
     this.state = {
-      sentences: null
+      sentences: tokenizeSentence(abstract),
+      sentencesHeights: null,
+      containerHeight: null
     };
   }
   componentDidMount() {
-    /*    console.log(this.refs.abstract1.offsetHeight);*/
-    // TODO: change it --> DUMMY DATA
-    const document = this.props.document;
-    let abstract = renderField(document, 'abstract');
-    abstract =
-      'African sleeping sickness is a tropical disease caused by Trypanosoma brucei gambiense or T. b. rhodesiense. Both subspecies are transmitted by the tsetse fly. In general, an infection is lethal without an effective treatment. We used a rodent efficacy model to test 4 compounds that had been previously identified in a novel in vitro screen as activators of parasite differentiation from bloodstream towards procyclic forms. The 4 compounds were trypanocidal in vitro. However, none of the compounds showed trypanocidal activity in vivo. Snapshot pharmacokinetic (PK) profiles indicated that the compound exposure was too low after intraperitoneal administration, which explains the lack of efficacy. Garden eels live in burrows from which they protrude their bodies to feed on planktonic organisms, show courtship behavior and reproduce, and in which they seek refuge from predators. Despite universal acceptance that garden eels retract into their burrows for predator avoidance, a surprising lack of published accounts of this behaviour exists. Here, opportunist observations made during shark abundance video surveys, show reactions of garden eels during encounters with potential predators and other large-bodied organisms. Brown garden eels (Heteroconger longissimus) were observed during ten encounters with larger fish, and showed variable responses to five different large-bodied species. Varied responses suggested an ability to discriminate between organisms and react according to relative predation risk and proximity. The largest reactions were in response to encounters with piscivorous teleosts, the most likely predators of garden eels. Multiple encounters with two species of sharks, both improbable predators, resulted in a less pronounced reaction, consistent across encounters but variable with proximity. An encounter with a non-predator teleost resulted in the mildest response, despite very close proximity. These observations suggest that garden eels have the ability to discriminate between large-bodied organisms, and react according to relative predation risk.';
-
-    const sentences = tokenizeSentence(abstract);
     const sentencesHeights = [];
+    this.state.sentences.map((sentence, i) => {
+      const ref = this.refs['abstract' + i.toString()];
+      if (ref) {
+        sentencesHeights.push({
+          id: 'abstract' + i,
+          height: ref.offsetHeight
+        });
+      }
+    });
+    this.setState({
+      sentencesHeights
+    });
 
-    // TODO: REFS IS RENDEREED AFTERWARDS
-    this.setState({sentences});
-  }
-  render() {
-    if (this.state.sentences) {
-      this.state.sentences.forEach((s, i) => {
-        const ref = this.refs['abstract' + i.toString()];
-        if (ref) {
-          console.log(ref.offsetHeight);
-        }
+    if (this.abstractContainer) {
+      this.setState({
+        containerHeight: this.abstractContainer.offsetHeight
       });
     }
+  }
 
+  render() {
     const field = 'abstract';
+    const containerId = field + 'Container';
+    console.log(this.state);
     return (
       <Container id={field}>
         <PreviewArticleTitleByField field={field} />
         <ReviewsWriterFieldContainer>
-          <Abstract>
-            {this.state.sentences ? (
-              this.state.sentences.map((sentence, i) => {
-                const id = field + i;
-                return (
-                  <span id={id} key={i} ref={id}>
-                    {sentence + ' '}
-                  </span>
-                );
-              })
-            ) : (
-              <GridSpinner />
-            )}
+          <Abstract
+            id={containerId}
+            innerRef={abstractContainer => {
+              this.abstractContainer = abstractContainer;
+            }}
+          >
+            {this.state.sentences.map((sentence, i) => {
+              const id = field + i;
+              return (
+                <span id={id} key={i} ref={id}>
+                  {sentence + ' '}
+                </span>
+              );
+            })}
           </Abstract>
           {this.props.isReview ? (
             <ReviewsWriterContainer field={field} {...this.props} />
