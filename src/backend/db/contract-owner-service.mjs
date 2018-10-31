58d700ee90dce9698035fd4acfd82414177c2356
severinwullschleger <severin.wullschleger@bluewin.ch>
2018-10-31 15:29:50 +0100

    replaced contract owner in db method

--- src/backend/db/contract-owner-service.mjs
+++ src/backend/db/contract-owner-service.mjs
@@ -1,35 +1,22 @@
-import ContractOwner from '../schema/contract-owner.mjs';
 import Journal from '../schema/journal.mjs';
-import {getContractOwner, getJournalParameters} from '../../smartcontracts/methods/web3-platform-contract-methods.mjs';
+import {getJournalParameters} from '../../smartcontracts/methods/web3-platform-contract-methods.mjs';
 
-export const getContractOwnerFromDB = () => {
-  return ContractOwner.findById(1);
-};
-
-export const writeContractOwnerInDB = async contract => {
-  const id = 1;
-  const contractOwnerAddress = await getContractOwner(contract);
-  let contractOwner = await ContractOwner.findById(id);
-  if (!contractOwner) {
-    contractOwner = new ContractOwner({
-      _id: id,
-      address: contractOwnerAddress
-    });
-  } else {
-    contractOwner.address = contractOwnerAddress;
-  }
-  return await contractOwner.save();
+export const getContractOwnerFromDB = async () => {
+  const journal = await Journal.findOne({
+    _id: 1
+  });
+  return journal.contractOwner;
 };
 
 export const saveJournalInformation = async contract => {
   let journal = await Journal.findOne({
-    contractAddress: contract.options.address
+    _id: 1
   });
   const journalInformationFromSC = await getJournalParameters(contract);
-  console.log(journalInformationFromSC);
 
   if (!journal) {
     journal = new Journal({
+      _id: 1,
       contractAddress: contract.options.address,
       contractOwner: journalInformationFromSC._contractOwner,
       minAmountOfEditorApprovedReviews: journalInformationFromSC._minAmountOfEditorApprovedReviews,
@@ -46,7 +33,6 @@ export const saveJournalInformation = async contract => {
       submissionFee: journalInformationFromSC._submissionFee,
       maxReviewRounds: journalInformationFromSC._maxReviewRounds
     });
-    console.log('new');
   }
   else {
     journal.contractOwner = journalInformationFromSC._contractOwner;
@@ -63,9 +49,6 @@ export const saveJournalInformation = async contract => {
     journal.secondReviewerRewardPerReviewer = journalInformationFromSC._secondReviewerRewardPerReviewer;
     journal.submissionFee = journalInformationFromSC._submissionFee;
     journal.maxReviewRounds = journalInformationFromSC._maxReviewRounds;
-
-    console.log(journal);
-    console.log('old');
   }
 
   return await journal.save();
--- src/backend/schema/journal.mjs
+++ src/backend/schema/journal.mjs
@@ -2,6 +2,10 @@ import mongoose from 'mongoose';
 
 export const journalSchema = mongoose.Schema(
   {
+    _id: {
+      type: Number,
+      required: true
+    },
     contractAddress: {
       type: String
     },
