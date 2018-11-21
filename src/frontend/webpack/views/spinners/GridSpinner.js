65e48c474c5ddcbebee4bfc2e10467e4acf24665
lucaspelloni2 <lucas_pelloni@hotmail.com>
2018-11-21 15:02:50 +0100

    spinner and styled components

--- src/frontend/webpack/views/spinners/GridSpinner.js
+++ src/frontend/webpack/views/spinners/GridSpinner.js
@@ -1,20 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
 import './grid.css';
 
-const GridSpinner = () => {
-	return (
-		<div className="sk-cube-grid">
-			<div className="sk-cube sk-cube1" />
-			<div className="sk-cube sk-cube2" />
-			<div className="sk-cube sk-cube3" />
-			<div className="sk-cube sk-cube4" />
-			<div className="sk-cube sk-cube5" />
-			<div className="sk-cube sk-cube6" />
-			<div className="sk-cube sk-cube7" />
-			<div className="sk-cube sk-cube8" />
-			<div className="sk-cube sk-cube9" />
-		</div>
-	);
+const Cube = styled.div`
+  background-color: ${props => (props.white ? 'white' : 'rgb(66, 113, 173)')};
+`;
+const GridSpinner = props => {
+  return (
+    <div className="sk-cube-grid">
+      <Cube className="sk-cube sk-cube1" {...props} />
+      <Cube className="sk-cube sk-cube2" {...props} />
+      <Cube className="sk-cube sk-cube3" {...props} />
+      <Cube className="sk-cube sk-cube4" {...props} />
+      <Cube className="sk-cube sk-cube5" {...props} />
+      <Cube className="sk-cube sk-cube6" {...props} />
+      <Cube className="sk-cube sk-cube7" {...props} />
+      <Cube className="sk-cube sk-cube8" {...props} />
+      <Cube className="sk-cube sk-cube9" {...props} />
+    </div>
+  );
 };
 
 export default GridSpinner;
