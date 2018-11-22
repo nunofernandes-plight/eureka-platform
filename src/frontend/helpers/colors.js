e8ce991db9c16125a956385268eae12a7a6a7807
lucaspelloni2 <lucas_pelloni@hotmail.com>
2018-11-22 16:49:19 +0100

    removed unused

--- src/frontend/helpers/colors.js
+++ src/frontend/helpers/colors.js
@@ -1,13 +1,8 @@
 export const __MAIN = '#4caef3';
-export const __MAIN_HOVER = '#72c5ff';
 export const __SECOND = '#4271ad';
-export const __SECOND_HOVER = '#4286c2';
 export const __THIRD = '#2e3292';
-export const __THIRD_HOVER = '#2e32a5';
 export const __FOURTH = '#66cad3';
-export const __FOURTH_HOVER = '#66d4dd';
 export const __FIFTH = '#5e72e4';
-export const __METAMASK = '#F6851B';
 
 // ALERT COLORS (for notifications and alert messages)
 export const __ALERT_ERROR = '#f75176';
