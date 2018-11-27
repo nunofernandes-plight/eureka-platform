2ba2288bb93de47e3d16c528034d15ea7fa5a450
lucaspelloni2 <lucas_pelloni@hotmail.com>
2018-11-27 16:38:04 +0100

    Poolmodal started

--- src/frontend/webpack/components/TxPool/PoolModal.js
+++ src/frontend/webpack/components/TxPool/PoolModal.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, {Fragment} from 'react';
+import styled, {keyframes} from 'styled-components';
+import {PANEL_LEFT_NORMAL_WIDTH} from '../../../helpers/layout.js';
 
 const Parent = styled.div`
-  display: flex;
   position: fixed;
   z-index: 1050;
   top: 0;
@@ -11,14 +11,46 @@ const Parent = styled.div`
   right: 0;
   overflow-x: hidden;
   overflow-y: auto;
-  background-color: ${props =>
-    props.show ? 'rgba(99, 114, 130, 0.5)' : 'transparent'};
-  visibility: ${props => (props.show ? 'visible' : 'hidden')};
-  transition: 0.35s all ease-in-out;
+  background-color: rgba(99, 114, 130, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const modalFade = keyframes`
+ from {transform: translateY(-70%);opacity: 0;}
+  to {transform: translateY(0);opacity: 1;}
+`;
+
+const MyModal = styled.div`
+  margin-left: ${PANEL_LEFT_NORMAL_WIDTH / 2}px;
+  height: 300px;
+  display: flex;
+  -ms-flex-direction: column;
+  position: fixed;
+  animation: ${modalFade};
+  animation-duration: 0.5s;
+  transition: 0.3s linear ease-in-out;
+  max-width: 100%;
+  max-height: 100%;
+  z-index: 12;
+  background: #fff;
+  border-radius: 4px;
+  box-shadow: 0 0 0 1px rgba(99, 114, 130, 0.16),
+    0 8px 16px rgba(27, 39, 51, 0.08);
+  background-clip: padding-box;
+  width: 560px;
 `;
 
 const PoolModal = ({show, ...otherProps}) => {
-  return <Parent show={show}>{otherProps.children}</Parent>;
+  return (
+    <Fragment>
+      {show ? (
+        <Parent>
+          <MyModal>{otherProps.children}</MyModal>
+        </Parent>
+      ) : null}
+    </Fragment>
+  );
 };
 
 export default PoolModal;
