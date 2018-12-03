0fe39c35d088cfb17201c683f550c3d660a9592d
severinwullschleger <severin.wullschleger@bluewin.ch>
2018-12-03 12:31:42 +0100

    new signed up for reviewing listener

--- src/backend/web3/contract-event-lister.mjs
+++ src/backend/web3/contract-event-lister.mjs
@@ -145,38 +145,23 @@ export default {
       }
     );
 
-    // EurekaPlatformContract.events.SignedUpForReviewing(
-    //   undefined,
-    //   async (error, event) => {
-    //     if (error) throw error;
-    //
-    //     const articleHash = event.returnValues.articleHash;
-    //     const reviewerAddress = event.returnValues.reviewerAddress;
-    //
-    //     let articleVersion = await ArticleVersion.findOne({
-    //       articleHash: articleHash
-    //     }).populate('editorApprovedReviews');
-    //     if (!articleVersion) errorThrower.noEntryFoundById(articleHash);
-    //
-    //     let articleReviews = articleVersion.editorApprovedReviews;
-    //     for (let i = 0; i < articleReviews.length; i++) {
-    //       if (articleReviews[i].reviewerAddress == reviewerAddress) {
-    //         articleReviews[i].stateTimestamp =
-    //           event.returnValues.stateTimestamp;
-    //         articleReviews[i].reviewState = ReviewState.SIGNED_UP_FOR_REVIEWING;
-    //         await articleReviews[i].save();
-    //         break;
-    //       }
-    //       if (i === articleReviews.length - 1) {
-    //         let error = new Error(
-    //           'Invitation Acception: corresponding review could not be found'
-    //         );
-    //         error.status = 500;
-    //         throw error;
-    //       }
-    //     }
-    //   }
-    // );
+    EurekaPlatformContract.events.SignedUpForReviewing(
+      undefined,
+      async (error, event) => {
+        if (error) throw error;
+
+        let reviewType;
+        if (event.returnValues.isEditorApprovedReview)
+          reviewType = ReviewType.EDITOR_APPROVED_REVIEW;
+        else
+          reviewType = ReviewType.COMMUNITY_REVIEW;
+
+        await reviewService.signUpForReviewing(
+          event.returnValues.reviewerAddress,
+          event.returnValues.articleHash,
+          reviewType)
+      }
+    );
 
     EurekaPlatformContract.events.EditorApprovedReviewIsAdded(
       undefined,
