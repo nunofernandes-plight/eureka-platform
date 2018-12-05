8ca6fc58dc05e9bec586e3bab5a08a3979e74ff8
lucaspelloni2 <lucas_pelloni@hotmail.com>
2018-12-05 14:14:38 +0100

    Contract overview draft

---
+++ src/frontend/webpack/components/SmartContractOwner/ContractOverview.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+
+class ContractOverview extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+
+}
