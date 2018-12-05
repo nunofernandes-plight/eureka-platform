750f251a49b91291a2650f83e65fd815f9a4901d
lucaspelloni2 <lucas_pelloni@hotmail.com>
2018-12-05 14:31:19 +0100

    Smart contract overview path

--- src/frontend/webpack/components/SmartContractOwner/ContractOverview.js
+++ src/frontend/webpack/components/SmartContractOwner/ContractOverview.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
+import {Card} from '../../views/Card.js';
+
+const Parent = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const Container = styled.div``;
 
 class ContractOverview extends React.Component {
   constructor() {
@@ -7,5 +15,15 @@ class ContractOverview extends React.Component {
     this.state = {};
   }
 
-
+  render() {
+    return (
+      <Parent>
+        <Card title={'Smart Contract Overview'}>
+          <Container> Implement the contract view here</Container>
+        </Card>
+      </Parent>
+    );
+  }
 }
+
+export default ContractOverview;
