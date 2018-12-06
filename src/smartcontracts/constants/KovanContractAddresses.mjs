1d374a881525a0b5f0bdc2e358a8cca9856acc7b
severinwullschleger <severin.wullschleger@bluewin.ch>
2018-12-06 13:06:58 +0100

    new kovan platform contract, token contract still the same

--- src/smartcontracts/constants/KovanContractAddresses.mjs
+++ src/smartcontracts/constants/KovanContractAddresses.mjs
@@ -1,3 +1,3 @@
 export const TOKEN_KOVAN_ADDRESS = '0xad9a8e2f14820f9c2a8f39bb501fba891f9f990a';
 export const PLATFORM_KOVAN_ADDRESS =
-  '0xad5d2e1a0a17be861dabcb3734c79bec688856ab';
+  '0x2eaf440187e2de48797d411a4c99b348b4f08a34';
