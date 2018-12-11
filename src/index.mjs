0afc015143c06e4a5399da462b012bfc016b2e02
lucaspelloni2 <lucas_pelloni@hotmail.com>
2018-12-11 09:28:25 +0100

    index to src root and package json updated

--- src/index.mjs
+++ src/index.mjs
@@ -1,11 +1,13 @@
-import app from './src/backend/api/api.mjs';
+import app from './backend/api/api.mjs';
 import dotenv from 'dotenv';
-import {isProduction} from './src/helpers/isProduction.mjs';
+import {isProduction} from './helpers/isProduction.mjs';
 if (!isProduction()) {
   //import env variables from .env file
   dotenv.config();
 }
-// TODO implement production
+
+
+
 app.setupApp();
 app.listenTo(process.env.PORT || 8080);
 console.log('App started. Date: ' + new Date().toString());
