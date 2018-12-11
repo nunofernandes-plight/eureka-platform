f5fd2960d0a227e39ea7700bf932770150272b8b
severinwullschleger <severin.wullschleger@bluewin.ch>
2018-12-11 16:17:23 +0100

    changed Address.js props and styling

--- src/frontend/webpack/views/Address.js
+++ src/frontend/webpack/views/Address.js
@@ -2,16 +2,26 @@ import React from 'react';
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 
-const Address = styled.div`
-  font-weight: bold;
+const Address = styled(Link)`
+  color: ${props => props.color};
+  font-weight: ${props => (props.fontWeight ? props.fontWeight : 'bold')};
+  font-size: ${props => props.fontSize};
+  word-break: break-all;
+  text-decoration: ${props => (props.noDecoration ? 'none' : null)};
 `;
 
-export const EthereumAddress = ({ethereumAddress, ...otherProps}) => {
+export const EthereumAddress = ({ethereumAddress, listLength, index, ...otherProps}) => {
   let content = otherProps.content;
   if (!content) content = ethereumAddress;
+  if (listLength && index === listLength - 2)
+    content = content + ' and ';
+  else if (listLength && index !== listLength -1)
+    content = content + ', ';
+
   return (
-    <Link to={`/app/users/${ethereumAddress}`}>
-      <Address>{content}</Address>
-    </Link>
+    <Address to={`/app/users/${ethereumAddress}`} {...otherProps}>
+      {content}
+    </Address>
   );
+
 };
