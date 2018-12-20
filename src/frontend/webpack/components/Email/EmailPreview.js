a9bf35286dd9a291dd6c3a5635996715572dcbd6
lucaspelloni2 <lucas_pelloni@hotmail.com>
2018-12-20 10:34:34 +0100

    Email template adjusted

--- src/frontend/webpack/components/Email/EmailPreview.js
+++ src/frontend/webpack/components/Email/EmailPreview.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import {__GRAY_200, __GRAY_700, __GRAY_900} from '../../../helpers/colors.js';
 import {renderField} from '../Articles/Online/TextEditor/DocumentRenderer.mjs';
+import {Email} from '../../views/Email.js';
 
 const Container = styled.div`
   margin-bottom: 30px;
@@ -18,16 +19,19 @@ const Line = styled.div`
   display: flex;
 `;
 
-const LineTitle = styled.h4`
+const LineTitle = styled.div`
   align-self: flex-start;
-  width: 55px;
   text-align: left;
-  margin: 0 10px 0 0;
+  font-weight: bold;
+  font-size: 12px;
+  margin: 0;
+  width: 52px;
 `;
 
 const LineContent = styled.div`
   color: ${__GRAY_900};
   text-align: left;
+  margin-right: 2.5px;
 `;
 
 const EmailContent = styled.p`
@@ -51,25 +55,32 @@ const EmailPreview = props => {
         <LineTitle>To:</LineTitle>
         {props.reviewersToInvite
           ? props.reviewersToInvite.map((r, i) => {
-              return <LineContent key={i}>{r.email}</LineContent>;
+              return (
+                <LineContent key={i}>
+                  <Email email={r.email} />
+                </LineContent>
+              );
             })
           : null}
       </Line>
       <Line>
-        <LineTitle>Cc:</LineTitle>
-      </Line>
-      <Line>
         <LineTitle>From:</LineTitle>
+        <LineContent>
+          <Email email={'ea-team@eurekatoken.io'} />
+        </LineContent>
       </Line>
       <Line>
         <LineTitle>Subject:</LineTitle>
-        <LineContent>Invitation for reviewing the article {title}</LineContent>
+        <LineContent style={{marginLeft: 6}}>
+          Invitation for reviewing the article {title}
+        </LineContent>
       </Line>
       <EmailContent>
         Dear Reviewer, <br />
-        You got an invitation for reviewing a manuscript titled {title}. Please
-        let the author now if you are willing to submit a review for this
-        manuscript by simply clicking the button below. <br />
+        You got an invitation for reviewing a manuscript titled{' '}
+        <strong>{title}</strong>. Please let the author now if you are willing
+        to submit a review for this manuscript by simply clicking the button
+        below. <br />
         <MyButton disabled>Accept invitation</MyButton>
         <br />
         Thanks a lot in advance for your time, <br />
