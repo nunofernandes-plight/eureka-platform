8b76aef6244006e1cb340c0202d4d598e14b63d6
lucaspelloni2 <lucas_pelloni@hotmail.com>
2019-01-21 12:09:35 +0100

    UserLookupRoutes

---
+++ src/frontend/webpack/components/Routers/UserLookupRoutes.js
@@ -0,0 +1,17 @@
+export const UserLookupRoutes = [
+  {
+    path: 'encoding',
+    name: 'Encoding',
+    icon: 'crypto'
+  },
+  {
+    path: 'publications',
+    icon: 'submitted',
+    name: 'Publications'
+  },
+  {
+    path: 'reviews',
+    icon: 'review',
+    name: 'Reviews'
+  }
+];
