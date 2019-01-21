862304a083997c203bad9ea44fbae56d40c070f9
lucaspelloni2 <lucas_pelloni@hotmail.com>
2019-01-21 11:46:16 +0100

    Tagline, separator, email and roles styled for user

--- src/frontend/webpack/views/User.js
+++ src/frontend/webpack/views/User.js
@@ -2,7 +2,15 @@ import React from 'react';
 import styled from 'styled-components';
 import Avatar from './Avatar.js';
 import UserRoles from './UserRoles.js';
-import {Email} from './Email.js';
+import {
+  __ALERT_ERROR,
+  __FIFTH,
+  __GRAY_400,
+  __GRAY_600,
+  __THIRD
+} from '../../helpers/colors.js';
+import {DraftsNavPillRoutes} from '../components/Routers/DraftsNavPillRoutes.js';
+import NavPill from './NavPill.js';
 
 const Container = styled.div`
   display: flex;
@@ -11,17 +19,45 @@ const Container = styled.div`
   flex-direction: column;
 `;
 
+const Email = styled.h2`
+  font-size: 1.2rem;
+  font-weight: 600;
+  letter-spacing: 0.025em;
+  color: ${__ALERT_ERROR} !important;
+  margin-bottom: 5px;
+`;
+
+const TagLine = styled.div`
+  color: ${__GRAY_600};
+  text-align: center;
+  max-width: 350px;
+  margin-bottom: 15px;
+`;
+
+const HorizontalSeparator = styled.div`
+  width: 15%;
+  height: 2px;
+  margin-top: 15px;
+  background: ${__GRAY_600};
+  align-self: center;
+  border-radius: 15px;
+`;
+
 const User = ({user}) => {
   return (
     <Container>
       <Avatar avatar={user.avatar} width={135} height={135} />
-      <Email
-        email={user.email}
-        fontSize={'20px'}
-        noDecoration
-        style={{margin: 14}}
-      />
+      <Email>{user.email}</Email>
+      <TagLine>
+        <em>
+          "Science is the key to our future, and if you don’t believe in
+          science, then you are holding everyone back."
+        </em>
+        – <strong>Bill Nye</strong>
+      </TagLine>
       <UserRoles roles={user.roles} />
+      <HorizontalSeparator />
+
     </Container>
   );
 };
--- src/frontend/webpack/views/UserRoles.js
+++ src/frontend/webpack/views/UserRoles.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import styled from 'styled-components';
 import {Role} from './Role.js';
+import {__FIFTH, __GRAY_100, __GRAY_200, __THIRD} from '../../helpers/colors.js';
 
 const Container = styled.div`
   display: flex;
   flex-wrap: wrap;
 `;
 
+const RoleContainer = styled.div`
+  display: flex;
+`;
+
+const Separator = styled.div`
+  width: 1px;
+  background: ${__GRAY_200};
+`;
+
 const UserRoles = ({roles}) => {
   return (
     <Container>
       {roles.map((role, i) => {
-        return <Role role={role} key={i} />;
+        return (
+          <RoleContainer>
+            <Role role={role} key={i} />
+            {i !== roles.length - 1 ? <Separator /> : null}
+          </RoleContainer>
+        );
       })}
     </Container>
   );
