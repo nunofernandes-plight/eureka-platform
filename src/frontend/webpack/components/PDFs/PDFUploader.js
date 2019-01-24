935e67d3f7c5c68311c724843e2f31b90db0ec1c
lucaspelloni2 <lucas_pelloni@hotmail.com>
2019-01-24 10:19:52 +0100

    isDragActive is styled

--- src/frontend/webpack/components/PDFs/PDFUploader.js
+++ src/frontend/webpack/components/PDFs/PDFUploader.js
@@ -25,67 +25,66 @@ const StyledDropzone = styled(Dropzone)`
   width: 100%;
 `;
 
+const DropperContent = styled.div`
+  color: ${__GRAY_300};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  transition: 0.2s ease-in-out;
+`;
+
 const Dropper = styled.div`
   &:hover {
     border: 2px dashed ${__GRAY_500};
+    & ${DropperContent} {
+      color: ${__GRAY_500};
+      & > svg {
+        fill: ${__GRAY_500};
+      }
+    }
   }
+
+  & {
+    ${props =>
+      props.isDragActive
+        ? `
+          border: 2px dashed ${__FIFTH};
+           & ${DropperContent} {
+              color: ${__FIFTH};   
+               & > svg {
+                  fill: ${__FIFTH};
+               }
+           }
+          `
+        : null}
+  }
+
+  border: 2px dashed ${__GRAY_300};
   display: flex;
   justify-content: center;
   align-items: center;
   flex: 1;
   border-radius: 20px;
-
-  border: ${props =>
-    !props.isDragActive ? `2px dashed ${__GRAY_300}` : `2px dashed ${__FIFTH}`};
   min-height: 150px;
   height: 100%;
   cursor: pointer;
   transition: 0.2s ease-in-out;
 `;
 
-const DropperContent = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-`;
-
 const DropperTitle = styled.h2`
-  color: ${props => (props.isHover ? `${__GRAY_500}` : `${__GRAY_300}`)};
   margin-top: 4px;
 `;
 
 class PDFUploader extends React.Component {
-  constructor() {
-    super();
-    this.state = {isHover: false};
-  }
   onDrop = (acceptedFiles, rejectedFiles) => {
     // Do something with files
   };
 
-  getIconColor(isActive) {
-    if (this.state.isHover) {
-      return __GRAY_500;
-    }
-    if (isActive) {
-      return __FIFTH;
-    }
-    return __GRAY_300;
-  }
-
   render() {
     return (
       <DropZoneContainer>
-        <StyledDropzone
-          onDrop={this.onDrop.bind(this)}
-          onMouseEnter={() => {
-            this.setState({isHover: true});
-          }}
-          onMouseLeave={() => {
-            this.setState({isHover: false});
-          }}
-        >
+        <StyledDropzone onDrop={this.onDrop.bind(this)}>
           {({isDragActive}) => {
             return (
               <Dropper isDragActive={isDragActive}>
@@ -95,14 +94,9 @@ class PDFUploader extends React.Component {
                     icon={'uploadPDF'}
                     width={75}
                     height={75}
-                    color={this.getIconColor(isDragActive)}
+                    color={__GRAY_300}
                   />
-                  <DropperTitle
-                    isHover={this.state.isHover}
-                    isDragActive={this.state.isDragActive}
-                  >
-                    Upload your PDF here
-                  </DropperTitle>
+                  <DropperTitle>Upload your PDF here</DropperTitle>
                 </DropperContent>
               </Dropper>
             );
